d88da20685ce3d0f0669ee9176cae4a45de083d9
Jesper Christensen <jesper@kalliope.org>
2003-05-10 15:50:55 +0000

    Box nedarver nu Mesh direkte

--- src/box.cpp
+++ src/box.cpp
@@ -13,7 +13,8 @@
 #include "constants.h"
 #include "mesh.h"
 
-Box::Box(const Vector a, const Vector b, Material mat) {
+Box::Box(const Vector a, const Vector b, Material mat) : Mesh(Mesh::MESH_FLAT,mat) {
+    
     Vector c2 = Vector(min(a[0],b[0]),min(a[1],b[1]),min(a[2],b[2]));
     Vector c7 = Vector(max(a[0],b[0]),max(a[1],b[1]),max(a[2],b[2]));
     
@@ -24,16 +25,24 @@ Box::Box(const Vector a, const Vector b, Material mat) {
     Vector c5 = Vector(c7[0],c2[1],c7[2]);
     Vector c6 = Vector(c2[0],c2[1],c7[2]);
     Vector c8 = Vector(c2[0],c7[1],c7[2]);
-    mesh = new Mesh(Mesh::MESH_FLAT,mat);
+    /*
+    //mesh = new Mesh(Mesh::MESH_FLAT,mat);
     mesh->addTriangle(c1,c2,c3); mesh->addTriangle(c2,c4,c3);
     mesh->addTriangle(c2,c6,c4); mesh->addTriangle(c6,c8,c4);
     mesh->addTriangle(c5,c6,c2); mesh->addTriangle(c1,c5,c2);
     mesh->addTriangle(c6,c5,c7); mesh->addTriangle(c6,c7,c8);
     mesh->addTriangle(c5,c1,c7); mesh->addTriangle(c1,c3,c7);
     mesh->addTriangle(c3,c4,c7); mesh->addTriangle(c4,c8,c7);
+    */
+    Mesh::addTriangle(c1,c2,c3); Mesh::addTriangle(c2,c4,c3);
+    Mesh::addTriangle(c2,c6,c4); Mesh::addTriangle(c6,c8,c4);
+    Mesh::addTriangle(c5,c6,c2); Mesh::addTriangle(c1,c5,c2);
+    Mesh::addTriangle(c6,c5,c7); Mesh::addTriangle(c6,c7,c8);
+    Mesh::addTriangle(c5,c1,c7); Mesh::addTriangle(c1,c3,c7);
+    Mesh::addTriangle(c3,c4,c7); Mesh::addTriangle(c4,c8,c7);
 }
 
-Box::Box(const Vector c, double width, double height, double depth, Material m) {
+Box::Box(const Vector c, double width, double height, double depth, Material m): Mesh(Mesh::MESH_FLAT,m) {
     width /= 2;
     height /= 2;
     depth /= 2;
@@ -43,6 +52,6 @@ Box::Box(const Vector c, double width, double height, double depth, Material m) 
 }
 
 Box::~Box() {
-    delete mesh;
+    // Do we leak stuff inside Mesh here?
 }
 
--- src/box.h
+++ src/box.h
@@ -1,17 +1,13 @@
 #ifndef BOX_H
 #define BOX_H
 
-#include "vector.h"
-#include "object.h"
 #include "mesh.h"
 
-class Intersection;
-class Ray;
-class Matrix;
 class Material;
+class Vector;
 
 /// A box
-class Box : public object {
+class Box : public Mesh {
 
     public:
 	/// Constructs a box with extremities at corner1 and corner2
@@ -20,25 +16,6 @@ class Box : public object {
 	Box(const Vector center, double width, double height, double depth, Material m);
 	/// Destructor
 	~Box();
-
-	virtual void transform(const Matrix& m) { mesh->transform(m); };
-	virtual Vector normal(const Intersection & i) { return mesh->normal(i); };
-	virtual RGB getDiffuseColor(const Vector& p) { return mesh->getDiffuseColor(p); };
-	virtual Material getMaterial() { return mesh->getMaterial(); };
-
-	virtual bool onEdge(const Vector &p) { return mesh->onEdge(p); };
-	virtual bool inside(const Vector &p) { return mesh->inside(p); };
-
-	virtual bool intersects(const BoundingBox& b) { return mesh->intersects(b); };
-	virtual BoundingBox boundingBoundingBox() {return mesh->boundingBoundingBox(); };
-
-	void getUV(const Intersection& intersection, double* u, double* v) { mesh->getUV(intersection, u, v); };
-
-
-    private:
-	Mesh* mesh;
-	Intersection _intersect(const Ray& ray) { return mesh->intersect(ray); };
-
 };
 
 #endif
