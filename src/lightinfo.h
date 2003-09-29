9b2cced135edc3486e3d2be639bdcd5b34923a30
Jesper Christensen <jesper@kalliope.org>
2003-09-29 14:57:26 +0000

    New file

---
+++ src/lightinfo.h
@@ -0,0 +1,17 @@
+#ifndef LIGHTINFO_H
+#define LIGHTINFO_H
+
+/// The datavalue class lightsources returns to the camera
+class Lightinfo {
+
+    public:
+
+	/// The intensity at a point by one lightsource
+	double intensity;
+	/// The cosine of the angle betweeen the incidentpoints normal and the ray towards the light
+	double cos;
+	/// The direction from the incidentpoint to the light
+	Vector direction_to_light;
+};
+
+#endif 
