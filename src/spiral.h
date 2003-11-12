2a90df602c0b8f0951fd50c33f9075e3c5df6352
Jesper Christensen <jesper@kalliope.org>
2003-11-12 15:12:30 +0000

    Ny path klasse

---
+++ src/spiral.h
@@ -0,0 +1,41 @@
+
+#ifndef SPIRAL_H
+#define SPIRAL_H
+
+#include "path.h"
+
+class Vector;
+class Matrix;
+class Circle;
+
+class Spiral : public Path {
+
+    public:
+
+	/// Constructor
+	Spiral(Path* path, double radius, double windings);
+	
+	/// Constructor
+	Spiral(Path* path, double radius, double windings, double offset);
+
+	/// Get a point on the path where t in [0,1]
+	Vector getPoint(double t) const;
+	
+	/// Get a tangent to the path where t in [0,1]
+	Vector getTangent(double t) const;
+
+	/// Transform the path
+	void transform(const Matrix& m);
+
+	static void test();
+
+    private:
+
+	Path* center;
+        double radius;
+	double windings;
+	double offset;
+	
+};
+
+#endif
