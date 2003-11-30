ab36fd7962f5ef36b3b8865d32a818ff4feaafe2
Jesper Christensen <jesper@kalliope.org>
2003-11-30 19:51:49 +0000

    Fix chrome

--- src/materials/materials.h
+++ src/materials/materials.h
@@ -7,8 +7,8 @@
 #define MATERIAL_SHINY_RED Material(RGB(1.0,0.2,0.2),0.75,RGB(1.0,1.0,1.0),0.20,30)
 #define MATERIAL_SHINY_GREEN Material(RGB(0.2,1.0,0.2),0.75,RGB(1.0,1.0,1.0),0.75,30)
 #define MATERIAL_SHINY_BLUE Material(RGB(0.2,0.2,1.0),0.75,RGB(1.0,1.0,1.0),0.20,30)
-#define MATERIAL_CHROME Material(RGB(0.8,0.8,0.8),0.7,RGB(1.0,1.0,1.0),0.80,40)
+#define MATERIAL_CHROME Material(RGB(0.8,0.8,0.8),0.2,RGB(1.0,1.0,1.0),0.80,50)
 
-#define MATERIAL_SHINY(rgb) Material(rgba,0.75,RGB(1.0,1.0,1.0),0.30,30)
+#define MATERIAL_SHINY(rgb) Material(rgb,0.75,RGB(1.0,1.0,1.0),0.30,30)
 
 #endif
