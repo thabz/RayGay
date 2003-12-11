7cc3bf163f9c37c31582dafbfaa8b2b8dc7994b3
Jesper Christensen <jesper@kalliope.org>
2003-12-11 09:25:28 +0000

    Compiler options

---
+++ src/tests/testspeed.cpp
@@ -0,0 +1,53 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdlib.h>
+#include <iostream>
+#include <cassert>
+#include <time.h>
+
+#define NUM_MULTS 1000000000
+#define NUM_DIVS 1000000000
+
+using namespace std;
+
+void float_test() {
+    float v = 1.0;
+    for(long i = 0; i < NUM_MULTS; i++) {
+	v *= 1.0000001;
+    }
+    cout << v << endl;
+    v = 1.0;
+    for(long i = 0; i < NUM_DIVS; i++) {
+	v /= 3.14;
+    }
+    cout << v << endl;
+}
+
+void double_test() {
+    double v = 1.0;
+    for(long i = 0; i < NUM_MULTS; i++) {
+	v *= 1.0000001;
+    }
+    cout << v << endl;
+    v = 1.0;
+    for(long i = 0; i < NUM_DIVS; i++) {
+	v /= 3.14;
+    }
+    cout << v << endl;
+
+}
+
+int main(int argc, char *argv[]) {
+    time_t beginTime;
+    
+    beginTime = time(NULL);
+    float_test();
+    printf("Float test took %ld seconds.\n",time(NULL) - beginTime);
+    beginTime = time(NULL);
+    double_test();
+    printf("Double test took %ld seconds.\n",time(NULL) - beginTime);
+    return EXIT_SUCCESS;
+}
+
