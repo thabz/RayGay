f2b6c8e4a8711a8b433b2242240b1b8660fe3bd5
Jesper Christensen <jesper@kalliope.org>
2003-12-18 09:28:16 +0000

    Another testcase ... for learning C++

---
+++ test/testlanguage.cpp
@@ -0,0 +1,34 @@
+/*
+ * This is a scratchpad for testing different C++ features...
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdlib.h>
+#include <cassert>
+#include <iostream>
+
+void test_bool() {
+    bool t1 = true;
+    bool t2 = true;
+    bool f1 = false;
+    bool f2 = false;
+
+    assert(t1 && t2 == true);
+    assert(t1 && t2);
+    assert(t1 || f1);
+    assert(f1 || t1);
+
+
+}
+
+int main(int argc, char *argv[]) {
+
+    test_bool();
+    return EXIT_SUCCESS;
+}
+
+
