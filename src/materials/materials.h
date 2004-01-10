34524a7db2c9cd5cfb03512ca4929488ae715707
Jesper Christensen <jesper@kalliope.org>
2004-01-10 21:19:47 +0000

    Dull blue added

--- src/materials/materials.h
+++ src/materials/materials.h
@@ -7,6 +7,7 @@
 #define MATERIAL_SHINY_RED Material(RGB(1.0,0.2,0.2),0.75,RGB(1.0,1.0,1.0),0.20,30)
 #define MATERIAL_SHINY_GREEN Material(RGB(0.2,1.0,0.2),0.75,RGB(1.0,1.0,1.0),0.75,30)
 #define MATERIAL_SHINY_BLUE Material(RGB(0.2,0.2,1.0),0.75,RGB(1.0,1.0,1.0),0.20,30)
+#define MATERIAL_DULL_BLUE Material(RGB(0.2,0.2,1.0),1.00,RGB(1.0,1.0,1.0),0.0,30)
 #define MATERIAL_CHROME Material(RGB(0.8,0.8,0.8),0.2,RGB(1.0,1.0,1.0),0.80,50)
 
 #define MATERIAL_SHINY(rgb) Material(rgb,0.75,RGB(1.0,1.0,1.0),0.30,30)
