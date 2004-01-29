6c4a1b9d8096d3f3c6b52ff509b491c22ab6b0a4
Jesper Christensen <jesper@kalliope.org>
2004-01-29 15:23:39 +0000

    Docs

--- src/paths/arc.cpp
+++ src/paths/arc.cpp
@@ -1,12 +1,28 @@
 
 #include "paths/arc.h"
 
+/**
+ * Constructor
+ *
+ * @param center center of the circle
+ * @param radius radius of the circle 
+ * @param normal normal to the plane the circle lies in
+ * @param begin_degree begin degree of the arc in degrees
+ * @param end_degree end degree of the arc in degrees
+ */
 Arc::Arc(const Vector& center, double radius, const Vector& normal, double begin_degree, double end_degree) {
     circle = new Circle(center,radius,normal);
-    begin_t = begin_degree / M_2PI;
-    end_t = end_degree / M_2PI;
+    begin_t = DEG2RAD(begin_degree) / M_2PI;
+    end_t = DEG2RAD(end_degree) / M_2PI;
 }
 
+/**
+ * Constructor
+ *
+ * @param circle the circle whose perimiter the arc should lie in
+ * @param begin_degree begin degree of the arc in degrees
+ * @param end_degree end degree of the arc in degrees
+ */
 Arc::Arc(Circle* circle, double begin_degree, double end_degree) {
     this->circle = circle;
     begin_t = begin_degree / M_2PI;
