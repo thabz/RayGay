66b36f6f8cf86d43c937e1806e782b7da2e5afac
Jesper Christensen <jesper@kalliope.org>
2004-02-15 21:18:49 +0000

    More

--- src/kdtree.cpp
+++ src/kdtree.cpp
@@ -1,10 +1,71 @@
 
+#include "kdtree.h"
+#include "intersection.h"
+#include "ray.h"
+#include "object.h"
+#include "ray.h"
+#include "stats.h"
 
-intersect(KdNode* rootNode, const Ray& ray, double a, double b) {
+Object* KdTree::last_primary_intersected_object = NULL;
+
+KdTree::KdTree() {
+    prepared = false;
+}
+
+KdTree::~KdTree() {
+    if (prepared) {
+	delete[] stack;
+    }
+}
+
+void KdTree::addObject(Object* obj) {
+    Stats::getUniqueInstance()->inc("KdTree: Objects added");
+    objects.push_back(obj);
+}
+
+inline
+bool KdTree::intersect(const Ray& ray) const {
+    ray.lowest_t = HUGE_DOUBLE;
+    return intersect(ray,double(0),HUGE_DOUBLE);
+}
+
+inline
+bool KdTree::intersectPrimary(const Ray& ray) const {
+    return intersect(ray,double(0),HUGE_DOUBLE);
+}
+
+inline
+bool KdTree::intersectForShadow(const Ray& ray) const {
+    return intersectForShadow(ray,double(0),HUGE_DOUBLE);
+}
+
+bool KdTree::intersectForShadow(const Ray& ray, const Object* hint) const {
+    if (hint != NULL && hint->intersect(ray)) {
+	last_intersection = hint->getLastIntersection();
+	return true;
+    } else {
+	return intersectForShadow(ray);
+    }
+}
+
+// TODO: Write a faster intersect for shadows
+bool KdTree::intersectForShadow(const Ray& ray,double a, double b) const {
+    return intersect(ray,a,b);
+}
+
+void KdTree::prepare() {
+    // TODO: Find max depth
+    stack = new StackElem[10000];
+}
+
+/**
+ * Implementation of the recursive $f[ TA_rec^B $f] algorithm.
+ */
+bool KdTree::intersect(const Ray& ray, double a, double b) const {
+
     double t;
-    StackElem stack[MAX_DEPTH];
-    KdNode *farChild, *currNode;
-    curNode = rootNode;
+    KdNode *farChild, *curNode;
+    curNode = nodes;
     int enPt = 0;
     stack[enPt].t = a;
 
@@ -19,9 +80,10 @@ intersect(KdNode* rootNode, const Ray& ray, double a, double b) {
     stack[exPt].node = NULL;
 
     while (curNode != NULL) {
-	while (curNode.axis >= 0) {
+	while (curNode->axis >= 0) {
 	    /* Current node is not a leaf */
 	    double splitVal = curNode->splitPlane;
+	    int axis = curNode->axis; // ?
 
 	    if (stack[enPt].pb[axis] <= splitVal) {
 		if (stack[exPt].pb[axis] <= splitVal) {
@@ -63,14 +125,26 @@ intersect(KdNode* rootNode, const Ray& ray, double a, double b) {
 
 	// TODO: Intersect with all objects in list, discarding
 	// those lying before stack[enPt].t or farther than stack[exPt].t
-	
-	if ("any intersected")
-	    return "closest";
+	bool object_hit = false;
+	if (curNode->objects->size() > 0) {
+	    const vector<Object*> objects = *(curNode->objects);
+	    double smallest_t = HUGE_DOUBLE;
+	    for (unsigned int i = 0; i < objects.size(); i++) {
+		if (objects[i]->intersect(ray) && 
+			objects[i]->getLastIntersection()->getT() < smallest_t) {
+		    last_intersection = objects[i]->getLastIntersection();
+		    smallest_t = last_intersection->getT();
+		    object_hit = true;
+		}
+	    }
+	    ray.lowest_t = min(ray.lowest_t,smallest_t);
+	}
+	if (object_hit) return true;
 	
 	enPt = exPt;
 
 	curNode = stack[exPt].node;
 	exPt = stack[enPt].prev;
     } /* while curNode != end of nodes */
-    return "no object"
+    return false;
 }
--- src/kdtree.h
+++ src/kdtree.h
@@ -1,10 +1,35 @@
 
+#ifndef KD_TREE
+#define KD_TREE
 
+#include "math/vector.h"
+#include "spacesubdivider.h"
+#include <vector>
+
+class Ray;
+class Intersection;
+class Object;
+
+/**
+ * k-dimensional tree
+ */
 class KdTree : public SpaceSubdivider {
+
+    public:
+	KdTree();
+	virtual ~KdTree();
+	bool intersect(const Ray& ray) const; ///< Returns the nearest intersection
+	bool intersectPrimary(const Ray& ray) const; ///< Returns the nearest intersection
+	bool intersectForShadow(const Ray& ray) const; ///< Returns any intersection 
+	bool intersectForShadow(const Ray& ray, const Object* hint) const; ///< Returns any intersection but hint-object is checked for intersection first.
+
+	Intersection* getLastIntersection() const { return last_intersection; };
+	void addObject(Object* obj); ///< Place a object in the kd-tree 
+	void prepare();
 
     private:
 	struct KdNode {
-	    Object* objlist;  // Enclosed objects when this is a leaf
+	    std::vector<Object*>* objects;  // Enclosed objects when this is a leaf
 	    KdNode* left;     // Left child
 	    KdNode* right;    // Right child
 	    float splitPlane; // Position of splitting plane
@@ -18,10 +43,18 @@ class KdTree : public SpaceSubdivider {
 	    int prev;       // pointer to previus stack item
 	};
 
+	bool intersect(const Ray& ray, double a, double b) const;
+	bool intersectForShadow(const Ray&,double,double) const;
+
 	KdNode* nodes;
-}
+	mutable StackElem* stack;
 
-bool intersect_recurse(const Ray& ray, const double min_t, const double max_t) const {
+	std::vector<Object*> objects;
+	bool prepared;
+	mutable Intersection* last_intersection;
+	static Object* last_primary_intersected_object;
 
+};
 
-}
+#endif
+
