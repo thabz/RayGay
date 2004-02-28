5a7952847966590045b0d05eddac3a19e8055388
Jesper Christensen <jesper@kalliope.org>
2004-02-28 10:27:23 +0000

    New spline type added

---
+++ src/paths/catmullromspline.cpp
@@ -0,0 +1,17 @@
+
+#include "catmullromspline.h"
+#include "math/vector.h"
+#include "math/matrix.h"
+
+Vector CatmullRomSpline::getPoint(double t) const {
+
+}
+
+Vector CatmullRomSpline::getTangent(double t) const {
+
+}
+
+void transform(const Matrix& m) {
+
+}
+
---
+++ src/paths/catmullromspline.h
@@ -0,0 +1,57 @@
+
+#ifndef CATMULL_ROM_SPLINE
+#define CATMULL_ROM_SPLINE
+
+#include <vector>
+#include "paths/path.h"
+
+class Vector;
+class Matrix;
+
+/**
+ * The Catmull-Rom spline.
+ * 
+ * The characteristic feature of this spline interpolation is that the spline
+ * passes through all but the first and last control points. The curve is
+ * \f$ C^1 \f$ continuous which means that the first derivative has no 
+ * discontinuities in the tangent direction and magnitude. The curve is not
+ * \f$ C^2 \f$ continuous as the second derivative is a linear interpolation.
+ *
+ * The Catmull-Rom spline from the segment \f$P_i\f$ to \f$P_{i+1}\f$ is defined as
+ *
+ * \f[ f(t) = \frac{-t^3 + 2t^2 - t}{2}P_{i-1} +
+ *            \frac{3t^3 - 5t^2 + 2}{2}P_{i} +
+ *            \frac{-3t^3 + 4t^2 + t}{2}P_{i+1} +
+ *            \frac{t^3 - t^2}{2}P_{i+2}  \f]
+ *
+ * for \f$ t \in [0,1] \f$. The spline interpolates the positions \f$P_i\f$ to \f$P_{i+1}\f$ as \f$ f(0) = P_i \f$ and \f$ f(1) = P_{i+1} \f$.
+ *
+ * The derivative is 
+ *
+ * \f[ f'(t) = \frac{-3t^2 + 4t - 1}{2}P_{i-1} +
+ *            \frac{9t^2 - 10t}{2}P_{i} +
+ *            \frac{-9t^2 + 8t + 1}{2}P_{i+1} +
+ *            \frac{3t^2 - 2t}{2}P_{i+2}  \f]
+ *
+ * One see that 
+ *
+ * \f[ f'(0) = \frac{P_{i+1} - P_{i-1}}{2} \f]
+ * \f[ f'(1) = \frac{P_{i+2} - P_{i}}{2} \f]
+ *
+ * or that the tangent at the end of spline segments are the vector from the
+ * previous point to the current point.
+ */
+class CatmullRomSpline : public Path {
+    public:
+	CatmullRomSpline(const std::vector<Vector>& points);
+	Vector getPoint(double t) const;
+	Vector getTangent(double t) const;
+	void transform(const Matrix& m);
+
+    private:
+	int points_num;
+	std::vector<Vector> points;
+};
+
+#endif
+
