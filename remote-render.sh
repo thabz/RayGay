bd0c83adc587fcd45371d9c5e8bd24ff5a801b4e
Jesper Christensen <jesper@kalliope.org>
2004-03-07 17:07:33 +0000

    Script til at udføre renderings på andre maskiner

---
+++ remote-render.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+REMOTE_HOST=$1
+SCENE_FILE=$2
+REMOTE_DIR=remote-tracer
+REMOTE_TGA=remote.tga
+REMOTE_PNG=remote.png
+
+echo $REMOTE_HOST
+
+echo Copying binary and scene files to $REMOTE_HOST
+strip src/tracer
+ssh $REMOTE_HOST mkdir $REMOTE_DIR
+scp src/tracer $REMOTE_HOST:$REMOTE_DIR
+rsync -avz --delete scenes $REMOTE_HOST:$REMOTE_DIR
+ssh $REMOTE_HOST ./$REMOTE_DIR/tracer $REMOTE_DIR/scenes/`basename $SCENE_FILE` $REMOTE_DIR/$REMOTE_TGA
+scp $REMOTE_HOST:$REMOTE_DIR/$REMOTE_TGA .
+convert $REMOTE_TGA $REMOTE_PNG
+rm $REMOTE_TGA
+eog $REMOTE_PNG
