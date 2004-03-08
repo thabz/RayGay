cca5d6c82cdecf55a2fedaa13e81a3c69449e824
Jesper Christensen <jesper@kalliope.org>
2004-03-08 14:41:12 +0000

    New file

---
+++ src/photon/irradiancecache.cpp
@@ -0,0 +1,12 @@
+
+
+/**
+ * The weight function.
+ *
+ * Using Henrik Wann Jensens interpretation.
+ */
+double IrradianceCache::CacheNode::getWeight(const Vector& x, const Vector& n) const {
+    double d1 = (x - point).length() / hmd;
+    double d2 = sqrt(1.0 - n*normal);
+    return 1.0 / (d1 + d2);
+}
