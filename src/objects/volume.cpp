8892f9bf96655302a2f911e62fc698f330504bd7
Jesper Christensen <jesper@kalliope.org>
2004-03-18 18:49:34 +0000

    More

--- src/objects/volume.cpp
+++ src/objects/volume.cpp
@@ -7,7 +7,18 @@
  * @see http://www.exaflop.org/docs/marchcubes/ind.html
  */
 Intersection Volume::_intersect(const Ray& ray) const {
+    const BoundingBox& bbox = this->boundingBoundingBox();
+    Vector2 inout = bbox.intersect(ray);
+    double t_begin = inout[0];
+    double t_end = inout[1];
+    double t_step = (t_end - t_begin) / double(steps);
 
+    for(double t = t_begin; t <= t_end; t += t_step) {
+	if (this->inside(ray.getPoint(t))) {
 
+	}
+    }
 
+    // No intersection
+    return Intersection();
 }
