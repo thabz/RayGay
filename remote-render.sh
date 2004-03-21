8a0c0b1a7a49a1f8f5d91e64a275d1688463f2c9
Jesper Christensen <jesper@kalliope.org>
2004-03-21 10:10:10 +0000

    Using -C flag with scp

--- remote-render.sh
+++ remote-render.sh
@@ -11,10 +11,10 @@ echo $REMOTE_HOST
 echo Copying binary and scene files to $REMOTE_HOST
 strip src/tracer
 ssh $REMOTE_HOST mkdir $REMOTE_DIR
-scp src/tracer $REMOTE_HOST:$REMOTE_DIR
+scp -C src/tracer $REMOTE_HOST:$REMOTE_DIR
 rsync -avz --delete scenes $REMOTE_HOST:$REMOTE_DIR
 ssh $REMOTE_HOST ./$REMOTE_DIR/tracer $REMOTE_DIR/scenes/`basename $SCENE_FILE` $REMOTE_DIR/$REMOTE_TGA
-scp $REMOTE_HOST:$REMOTE_DIR/$REMOTE_TGA .
+scp -C $REMOTE_HOST:$REMOTE_DIR/$REMOTE_TGA .
 convert $REMOTE_TGA $REMOTE_PNG
 rm $REMOTE_TGA
 eog $REMOTE_PNG
