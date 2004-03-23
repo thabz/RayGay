dc2a37a1e322527da812853c76e2d21320a76908
Jesper Christensen <jesper@kalliope.org>
2004-03-23 14:07:40 +0000

    Diffuse color implemented

--- src/materials/wood.cpp
+++ src/materials/wood.cpp
@@ -1,8 +1,20 @@
 
 #include "wood.h"
+#include "perlin.h"
+#include "intersection.h"
 
 Wood::Wood(const RGB& color1, const RGB& color2) : Material() {
     this->color1 = color1;
     this->color2 = color2;
+}
+
+RGB Wood::getDiffuseColor(const Intersection& i) const {
+    Vector p = i.getPoint();
+    float vec[3];
+    vec[0] = p[0];vec[1] = p[1];vec[1] = p[1];
+    float g = noise3(vec) * 20;
+    g = g - int(g);
+
+    return g*color1 + (1-g)*color2;
 }
 
