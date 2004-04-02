f9994c408993126a91345ac078fa1de420aefad5
Jesper Christensen <jesper@kalliope.org>
2004-04-02 11:36:51 +0000

    Output triangles with uv-coordinates

--- src/objects/box.cpp
+++ src/objects/box.cpp
@@ -20,29 +20,51 @@ Box::Box(const Vector c, double width, double height, double depth, Material* m)
 }
 
 void Box::prepareBox(const Vector a, const Vector b) {
+    // Back left bottom
     Vector c2 = Vector(min(a[0],b[0]),min(a[1],b[1]),min(a[2],b[2]));
+    // Front right top
     Vector c7 = Vector(max(a[0],b[0]),max(a[1],b[1]),max(a[2],b[2]));
     
+    // Back right bottom
     Vector c1 = Vector(c7[0],c2[1],c2[2]);
+    // Back left top
     Vector c4 = Vector(c2[0],c7[1],c2[2]);
+    // Back right top
     Vector c3 = Vector(c7[0],c7[1],c2[2]);
-
+    // Front right bottom
     Vector c5 = Vector(c7[0],c2[1],c7[2]);
+    // Front left bottom
     Vector c6 = Vector(c2[0],c2[1],c7[2]);
+    // Front left top
     Vector c8 = Vector(c2[0],c7[1],c7[2]);
 
-    Vector2 uv1 = Vector2(0.0,0.0);
-    Vector2 uv2 = Vector2(0.0,1.0);
-    Vector2 uv3 = Vector2(1.0,0.0);
-    Vector2 uv4 = Vector2(1.0,1.0);
-    // TODO: addTriangles with uv-coordinates
+    // Bottom left
+    Vector2 uv00 = Vector2(0.0,0.0);
+    // Top left
+    Vector2 uv01 = Vector2(0.0,1.0);
+    // Bottom right
+    Vector2 uv10 = Vector2(1.0,0.0);
+    // Top right
+    Vector2 uv11 = Vector2(1.0,1.0);
 
-    Mesh::addTriangle(c1,c2,c3); Mesh::addTriangle(c2,c4,c3);
-    Mesh::addTriangle(c2,c6,c4); Mesh::addTriangle(c6,c8,c4);
-    Mesh::addTriangle(c5,c6,c2); Mesh::addTriangle(c1,c5,c2);
-    Mesh::addTriangle(c6,c5,c7); Mesh::addTriangle(c6,c7,c8);
-    Mesh::addTriangle(c5,c1,c7); Mesh::addTriangle(c1,c3,c7);
-    Mesh::addTriangle(c3,c4,c7); Mesh::addTriangle(c4,c8,c7);
+    // Back face
+    Mesh::addTriangle(c1,c2,c3,uv00,uv10,uv01);
+    Mesh::addTriangle(c2,c4,c3,uv10,uv11,uv01);
+    // Left face
+    Mesh::addTriangle(c2,c6,c4,uv00,uv10,uv01); 
+    Mesh::addTriangle(c6,c8,c4,uv10,uv11,uv01);
+    // Bottom face
+    Mesh::addTriangle(c5,c6,c2,uv11,uv01,uv00); 
+    Mesh::addTriangle(c1,c5,c2,uv10,uv11,uv00);
+    // Front face
+    Mesh::addTriangle(c6,c5,c7,uv00,uv10,uv11); 
+    Mesh::addTriangle(c6,c7,c8,uv00,uv11,uv01);
+    // Right face
+    Mesh::addTriangle(c5,c1,c7,uv00,uv10,uv01); 
+    Mesh::addTriangle(c1,c3,c7,uv10,uv11,uv01);
+    // Top face
+    Mesh::addTriangle(c3,c4,c7,uv11,uv01,uv10); 
+    Mesh::addTriangle(c4,c8,c7,uv01,uv00,uv10);
 }
 
 Box::~Box() {
