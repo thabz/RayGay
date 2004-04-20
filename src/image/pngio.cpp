e754e2b628e6a232b4154dbb36b5cb5acba8e52c
Jesper Christensen <jesper@kalliope.org>
2004-04-20 10:54:59 +0000

    Woo! Loader and saver implemented and working.

--- src/image/pngio.cpp
+++ src/image/pngio.cpp
@@ -5,96 +5,205 @@
 #include <string>
 #include <cstdio>
 #include <memory.h>
- //#include <setjmp.h>
+//#include <setjmp.h>
 #include <png.h>
 
 using namespace std;
 
 void PngIO::save(const Image* const image, const std::string& filename) const {
-    cout << "PNG saving not implemented " << __FILE__ << __LINE__ << endl;
-    return;
+
+    FILE *fp;
+    png_structp png_ptr;
+    png_infop info_ptr;
+
+    int width = image->getWidth();
+    int height = image->getHeight();
+
+    /* open the file */
+    fp = fopen(filename.c_str(), "wb");
+    if (fp == NULL)
+	return; // TODO: Throw exception
+
+    /* Create and initialize the png_struct with the desired error handler
+     * functions.  If you want to use the default stderr and longjump method,
+     * you can supply NULL for the last three parameters.  We also check that
+     * the library version is compatible with the one used at compile time,
+     * in case we are using dynamically linked libraries.  REQUIRED.
+     */
+    png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING,
+	    NULL, NULL, NULL);
+
+    if (png_ptr == NULL)
+    {
+	fclose(fp);
+	return; // TODO: Throw exception
+    }
+
+    /* Allocate/initialize the image information data.  REQUIRED */
+    info_ptr = png_create_info_struct(png_ptr);
+    if (info_ptr == NULL)
+    {
+	fclose(fp);
+	png_destroy_write_struct(&png_ptr,  png_infopp_NULL);
+	return; // TODO: Throw exception
+    }
+
+    /* Set error handling.  REQUIRED if you aren't supplying your own
+     * error handling functions in the png_create_write_struct() call.
+     */
+    if (setjmp(png_jmpbuf(png_ptr)))
+    {
+	/* If we get here, we had a problem reading the file */
+	fclose(fp);
+	png_destroy_write_struct(&png_ptr, &info_ptr);
+	return; // TODO: Throw exception
+    }
+
+    png_init_io(png_ptr, fp);
+
+    png_set_IHDR(png_ptr, info_ptr, width, height, 8, 
+	    PNG_COLOR_TYPE_RGB_ALPHA, PNG_INTERLACE_NONE, 
+	    PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
+
+    /*
+       text_ptr[0].key = "Title";
+       text_ptr[0].text = "RayGay Image";
+       text_ptr[0].compression = PNG_TEXT_COMPRESSION_NONE;
+       text_ptr[1].key = "Author";
+       text_ptr[1].text = "RayGay";
+       text_ptr[1].compression = PNG_TEXT_COMPRESSION_NONE;
+       text_ptr[2].key = "Description";
+       text_ptr[2].text = "An image rendered with RayGay";
+       text_ptr[2].compression = PNG_TEXT_COMPRESSION_zTXt;
+#ifdef PNG_iTXt_SUPPORTED
+text_ptr[0].lang = NULL;
+text_ptr[1].lang = NULL;
+text_ptr[2].lang = NULL;
+#endif
+
+png_set_text(png_ptr, info_ptr, text_ptr, 3);
+*/
+    png_write_info(png_ptr, info_ptr);
+
+    /* If you are only writing one row at a time, this works */
+    png_byte row[width*4];
+    png_bytep rowp = row;
+
+    for (int y = 0; y < height; y++)
+    {
+	for(int x = 0; x < width; x++) {
+	    RGBA pixel = image->getRGBA(x,y);
+	    row[x*4 + 0] = int(pixel.r() * 255);
+	    row[x*4 + 1] = int(pixel.g() * 255);
+	    row[x*4 + 2] = int(pixel.b() * 255);
+	    row[x*4 + 3] = int(pixel.a() * 255);
+	}
+	png_write_rows(png_ptr, &rowp, 1);
+    }
+
+    png_write_end(png_ptr, info_ptr);
+
+    /* clean up after the write, and free any memory allocated */
+    png_destroy_write_struct(&png_ptr, &info_ptr);
+
+    /* close the file */
+    fclose(fp);
 }
 
 Image* PngIO::load(const std::string& filename) {
-   png_structp png_ptr;
-   png_infop info_ptr;
-   png_uint_32 width, height;
-   int bit_depth, color_type, interlace_type;
-   FILE *fp;
-   if ((fp = fopen(filename.c_str(), "rb")) == NULL)
-       return NULL;
+    png_structp png_ptr;
+    png_infop info_ptr;
+    png_uint_32 width, height;
+    int bit_depth, color_type, interlace_type;
+    FILE *fp;
+    if ((fp = fopen(filename.c_str(), "rb")) == NULL)
+	return NULL;
 
-   png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING,
-	   (png_voidp) NULL, NULL, NULL);
+    png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING,
+	    (png_voidp) NULL, NULL, NULL);
 
-   if (png_ptr == NULL)
-   {
-       fclose(fp);
-       return NULL;
-   }
+    if (png_ptr == NULL)
+    {
+	fclose(fp);
+	return NULL;
+    }
 
-   /* Allocate/initialize the memory for image information.  REQUIRED. */
-   info_ptr = png_create_info_struct(png_ptr);
-   if (info_ptr == NULL)
-   {
-      fclose(fp);
-      png_destroy_read_struct(&png_ptr, png_infopp_NULL, png_infopp_NULL);
-      return NULL;
-   }
+    /* Allocate/initialize the memory for image information.  REQUIRED. */
+    info_ptr = png_create_info_struct(png_ptr);
+    if (info_ptr == NULL)
+    {
+	fclose(fp);
+	png_destroy_read_struct(&png_ptr, png_infopp_NULL, png_infopp_NULL);
+	return NULL;
+    }
 
-   if (setjmp(png_jmpbuf(png_ptr)))
-   {
-      /* Free all of the memory associated with the png_ptr and info_ptr */
-      png_destroy_read_struct(&png_ptr, &info_ptr, png_infopp_NULL);
-      fclose(fp);
-      /* If we get here, we had a problem reading the file */
-      return (NULL);
-   }
+    if (setjmp(png_jmpbuf(png_ptr)))
+    {
+	/* Free all of the memory associated with the png_ptr and info_ptr */
+	png_destroy_read_struct(&png_ptr, &info_ptr, png_infopp_NULL);
+	fclose(fp);
+	/* If we get here, we had a problem reading the file */
+	return (NULL);
+    }
 
-   png_init_io(png_ptr, fp);
+    png_init_io(png_ptr, fp);
 
-   png_read_info(png_ptr, info_ptr);
+    png_read_info(png_ptr, info_ptr);
 
-   png_get_IHDR(png_ptr, info_ptr, &width, &height, &bit_depth, &color_type,
-       &interlace_type, int_p_NULL, int_p_NULL);
+    png_get_IHDR(png_ptr, info_ptr, &width, &height, &bit_depth, &color_type,
+	    &interlace_type, int_p_NULL, int_p_NULL);
 
-   /* Expand paletted colors into true RGB triplets */
-   /*
-   if (color_type == PNG_COLOR_TYPE_PALETTE)
-      png_set_palette_rgb(png_ptr);
-*/
-   /* Expand grayscale images to the full 8 bits from 1, 2, or 4 bits/pixel */
-   if (color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8)
-      png_set_gray_1_2_4_to_8(png_ptr);
+    /* Expand paletted colors into true RGB triplets */
+    /*
+       if (color_type == PNG_COLOR_TYPE_PALETTE)
+       png_set_palette_rgb(png_ptr);
+       */
+    /* Expand grayscale images to the full 8 bits from 1, 2, or 4 bits/pixel */
+    if (color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8)
+	png_set_gray_1_2_4_to_8(png_ptr);
 
-   /* Expand paletted or RGB images with transparency to full alpha channels
-    * so the data will be available as RGBA quartets.
-    */
-   if (png_get_valid(png_ptr, info_ptr, PNG_INFO_tRNS))
-      png_set_tRNS_to_alpha(png_ptr);
+    /* Expand paletted or RGB images with transparency to full alpha channels
+     * so the data will be available as RGBA quartets.
+     */
+    if (png_get_valid(png_ptr, info_ptr, PNG_INFO_tRNS))
+	png_set_tRNS_to_alpha(png_ptr);
 
-   png_read_update_info(png_ptr, info_ptr);
+    png_read_update_info(png_ptr, info_ptr);
 
-   /* Allocate the memory to hold the image using the fields of info_ptr. */
+    /* Allocate the memory to hold the image using the fields of info_ptr. */
 
-   /* The easiest way to read the image: */
-   png_bytep row = (png_bytep) png_malloc(png_ptr, png_get_rowbytes(png_ptr, info_ptr));
+    /* The easiest way to read the image: */
+ //   png_bytep row = (png_bytep) png_malloc(png_ptr, png_get_rowbytes(png_ptr, info_ptr));
+    png_byte row[png_get_rowbytes(png_ptr, info_ptr)];
+    png_bytep rowp = row;
 
-   for (unsigned int y = 0; y < height; y++)
-   {
-       png_read_rows(png_ptr, &row, png_bytepp_NULL, 1);
-   }
+    Image* result = new Image(width,height);
+    //cout << "Row bytes: " << png_get_rowbytes(png_ptr,info_ptr) << endl;
+    assert(png_get_rowbytes(png_ptr,info_ptr) == 4 * width);
 
-   png_read_end(png_ptr, info_ptr);
+    for (unsigned int y = 0; y < height; y++)
+    {
+	png_read_rows(png_ptr, &rowp, png_bytepp_NULL, 1);
+	for(unsigned int x = 0; x < width; x++) {
+	    RGBA col = RGBA(rowp[4*x + 0] / 255.0,
+		            rowp[4*x + 1] / 255.0,
+		            rowp[4*x + 2] / 255.0,
+		            rowp[4*x + 3] / 255.0);
+	    result->setRGBA(x,y,col);
+	}
+    }
 
-   /* clean up after the read, and free any memory allocated - REQUIRED */
-   png_destroy_read_struct(&png_ptr, &info_ptr, png_infopp_NULL);
+    png_read_end(png_ptr, info_ptr);
 
-   /* close the file */
-   fclose(fp);
+    /* clean up after the read, and free any memory allocated - REQUIRED */
+    png_destroy_read_struct(&png_ptr, &info_ptr, png_infopp_NULL);
 
-   /* that's it */
-   return NULL;
+    /* close the file */
+    fclose(fp);
 
+    /* that's it */
+    return result;
 }
+
+
 
