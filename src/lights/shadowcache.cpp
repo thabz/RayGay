b7a51476dc98ef00c148f80b5616fe6b31a9a06f
Jesper Christensen <jesper@kalliope.org>
2004-05-13 13:44:32 +0000

    Beginning support for voxel-checking

--- src/lights/shadowcache.cpp
+++ src/lights/shadowcache.cpp
@@ -7,35 +7,67 @@
 
 ShadowCache::ShadowCache() {
     for(int i = 0; i < LIGHTS_SHADOW_CACHE_MAX_DEPTH; i++) {
-	objectlist[i] = NULL;
+	hints[i] = NULL;
+	voxels[i] = NULL;
     }
 }
 
-Object* ShadowCache::get(unsigned int depth) const {
+Object* ShadowCache::getHint(unsigned int depth) const {
     if (depth < LIGHTS_SHADOW_CACHE_MAX_DEPTH) {
-	return objectlist[depth];
+	return hints[depth];
     } else {
 	return NULL;
     }
 }
 
-void ShadowCache::put(unsigned int depth, Object* object) {
+void ShadowCache::putHint(unsigned int depth, Object* object) {
     if (depth < LIGHTS_SHADOW_CACHE_MAX_DEPTH) {
-	objectlist[depth] = object;
+	hints[depth] = object;
     }
 }
 
+vector<Object*>* ShadowCache::getVoxel(unsigned int depth) const {
+    if (depth < LIGHTS_SHADOW_CACHE_MAX_DEPTH) {
+	return voxels[depth];
+    } else {
+	return NULL;
+    }
+}
+
+void ShadowCache::putVoxel(unsigned int depth, vector<Object*>* voxel) {
+    if (depth < LIGHTS_SHADOW_CACHE_MAX_DEPTH) {
+	voxels[depth] = voxel;
+    }
+}
+
+
 bool ShadowCache::occluded(const Ray& ray_to_light, const double dist_to_light, unsigned int depth, SpaceSubdivider* space) {
-    // TODO: Check other objects in hints voxel
-    Object* hint = get(depth);
+    // Check last shadowing object first
+    Object* hint = getHint(depth);
     if (hint != NULL) {
 	double t = hint->fastIntersect(ray_to_light);
 	if (t > 0 && t < dist_to_light) {
 	    return true;
 	}
     }
+    
+    // Check other objects in the same BSP-voxel
+    vector<Object*>* voxel = getVoxel(depth);
+    if (voxel != NULL) {
+	unsigned int size = voxel->size();
+	const vector<Object*> &objects = *(voxel);
+	for(unsigned int i = 0; i < size; i++) {
+           double t = objects[i]->fastIntersect(ray_to_light);
+	   if (t > 0 && t < dist_to_light) {
+               return true;
+	   }
+	}
+    }
+
+    // Finally do a real intersection test
     hint = space->intersectForShadow(ray_to_light,dist_to_light);
-    put(depth,hint);
+    putHint(depth,hint);
+    putVoxel(depth,NULL);
     return hint != NULL;
 }
 
--- src/lights/shadowcache.h
+++ src/lights/shadowcache.h
@@ -4,6 +4,10 @@
 
 #define LIGHTS_SHADOW_CACHE_MAX_DEPTH 32
 
+#include <vector>
+
+using namespace std;
+
 class Ray;
 class Object;
 
@@ -13,9 +17,15 @@ class ShadowCache {
 	bool occluded(const Ray& ray_to_light, const double dist_to_light, unsigned int depth, SpaceSubdivider* space);
 
     private:
-	Object* objectlist[LIGHTS_SHADOW_CACHE_MAX_DEPTH];
-	Object* get(unsigned int depth) const;
-	void put(unsigned int depth, Object* object);
+	// Methods
+	Object* getHint(unsigned int depth) const;
+	void putHint(unsigned int depth, Object* object);
+	vector<Object*>* getVoxel(unsigned int depth) const;
+	void putVoxel(unsigned int depth, vector<Object*>* voxel);
+
+	// Data
+	Object* hints[LIGHTS_SHADOW_CACHE_MAX_DEPTH];
+	vector<Object*>* voxels[LIGHTS_SHADOW_CACHE_MAX_DEPTH];
 };
 
 #endif
