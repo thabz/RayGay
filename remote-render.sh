01f7c4d933791d2fe17e6631183cdd716c6b514c
Jesper Christensen <jesper@kalliope.org>
2004-05-18 21:48:34 +0000

    No mkdir warning

--- remote-render.sh
+++ remote-render.sh
@@ -3,18 +3,14 @@
 REMOTE_HOST=$1
 SCENE_FILE=$2
 REMOTE_DIR=remote-tracer
-REMOTE_TGA=remote.png
 REMOTE_PNG=remote.png
 
 echo $REMOTE_HOST
 
 echo Copying binary and scene files to $REMOTE_HOST
 strip src/tracer
-ssh $REMOTE_HOST mkdir $REMOTE_DIR
-#scp -C src/tracer $REMOTE_HOST:$REMOTE_DIR
+ssh $REMOTE_HOST mkdir -p $REMOTE_DIR
 rsync -avz --delete src/tracer scenes $REMOTE_HOST:$REMOTE_DIR
-ssh $REMOTE_HOST ./$REMOTE_DIR/tracer $REMOTE_DIR/scenes/`basename $SCENE_FILE` $REMOTE_DIR/$REMOTE_TGA
-scp -C $REMOTE_HOST:$REMOTE_DIR/$REMOTE_TGA .
-#convert $REMOTE_TGA $REMOTE_PNG
-#rm $REMOTE_TGA
+ssh $REMOTE_HOST ./$REMOTE_DIR/tracer $REMOTE_DIR/scenes/`basename $SCENE_FILE` $REMOTE_DIR/$REMOTE_PNG
+scp -C $REMOTE_HOST:$REMOTE_DIR/$REMOTE_PNG .
 eog $REMOTE_PNG
