070058863df41d9a33e6b817cda03564df844f6b
Jesper Christensen <jesper@kalliope.org>
2004-05-29 05:34:13 +0000

    Print some sizes

--- test/testmath.cpp
+++ test/testmath.cpp
@@ -15,6 +15,14 @@
 using namespace std;
 
 void vector_test() {
+
+    cout << "Size of short: " << sizeof(short) << endl;
+    cout << "Size of int: " << sizeof(int) << endl;
+    cout << "Size of long: " << sizeof(long) << endl;
+    cout << "Size of float: " << sizeof(float) << endl;
+    cout << "Size of double: " << sizeof(double) << endl;
+    cout << "Size of long double: " << sizeof(long double) << endl;
+    cout << "Size of Vector: " << sizeof(Vector) << endl;
     
     Vector v = Vector(1,1,1);
     assert(IS_EQUAL(v.norm(),3));
