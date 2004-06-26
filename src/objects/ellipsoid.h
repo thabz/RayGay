79a08031d8eb12213fd75035cd0e77cd35e6fccf
Jesper Christensen <jesper@kalliope.org>
2004-06-26 05:30:33 +0000

    New object on the way

---
+++ src/objects/ellipsoid.h
@@ -0,0 +1,18 @@
+
+#ifndef OBJECTS_ELLIPSOID_H
+#define OBJECTS_ELLIPSOID_H
+
+#include "objects/solid.h"
+#include "objects/transformer.h"
+
+/**
+ * An ellipsoid object.
+ * 
+ * \todo Finish implementation.
+ */
+class Ellipsoid : public Solid, public Transformer {
+
+
+};
+
+#endif;
