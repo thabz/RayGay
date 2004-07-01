e9a9f004065259c6b9ce082da051024d41297776
Jesper Christensen <jesper@kalliope.org>
2004-07-01 17:33:55 +0000

    Speed up intersection

--- src/objects/csg.cpp
+++ src/objects/csg.cpp
@@ -102,23 +102,26 @@ void CSGDifference::allIntersections(const Ray& ray, vector<Intersection>& resul
 void CSGIntersection::allIntersections(const Ray& ray, vector<Intersection>& result) const {
     vector<Intersection> left_int;
     left->allIntersections(ray,left_int);
-    vector<Intersection> right_int;
-    right->allIntersections(ray,right_int);
-    result.reserve(left_int.size() + right_int.size());
-    unsigned int l = 0;
-    unsigned int r = 0;
     bool left_inside = false;
-    bool right_inside = false;
     if (left_int.size() > 0) {
 	left_inside = !left_int[0].isEntering();
     }
+    // Bail out quickly if possible
+    if (left_int.empty() && !left_inside) return;
+
+    vector<Intersection> right_int;
+    right->allIntersections(ray,right_int);
+    bool right_inside = false;
     if (right_int.size() > 0) {
 	right_inside = !right_int[0].isEntering();
     }
     // Bail out quickly if possible
-    if (left_int.empty() && !left_inside) return;
     if (right_int.empty() && !right_inside) return;
+    
     // Merge intersections while preserving order
+    result.reserve(left_int.size() + right_int.size());
+    unsigned int l = 0;
+    unsigned int r = 0;
     while (l < left_int.size() && r < right_int.size()) {
 	if (left_int[l].getT() < right_int[r].getT()) {
 	    Intersection i = left_int[l];
