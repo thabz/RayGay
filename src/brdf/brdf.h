198a54dbe9ed461ca1045babe82000154231c1b4
Jesper Christensen <jesper@kalliope.org>
2004-07-09 09:19:28 +0000

    More

--- src/brdf/brdf.h
+++ src/brdf/brdf.h
@@ -2,10 +2,19 @@
 #include "math/vector.h"
 
 /**
+ * Abstract superclass of all BRDFs.
+ * 
+ * Physically correct rendering systems describe reflection with
+ * a <i>bidirectional reflectance distribution function</i>. A BRDF
+ * is a function of a position on a surface and two directions, one
+ * of incoming light and one of outgoing light.
+ * 
+ * 
  * The BRDF of an isotropic material is invariant under rotation
  * around the normal. All other materials are called anisotropic.
  *
- * @see http://www.fas.harvard.edu/~lib175/projects_fall_2000/vernal/
+ * @see http://www.fas.harvard.edu/~lib175/projects_fall_2000/vernal/ for
+ * a examples of some of the bestknown BRDFs.
  */
 class BRDF {
 
