5be0d1ef78aabc61a01bcb3f41b7de8045a73bd2
Jesper Christensen <jesper@kalliope.org>
2004-07-09 09:14:54 +0000

    More

---
+++ src/brdf/ashikhmin_shirley.h
@@ -0,0 +1,10 @@
+
+/**
+ * An Anisotropic Phong BRDF Model
+ * 
+ * @see http://www.cs.utah.edu/~shirley/papers/jgtbrdf.pdf
+ */
+class AshikhminShirley : public BRDF {
+
+
+}
--- src/brdf/brdf.h
+++ src/brdf/brdf.h
@@ -4,6 +4,8 @@
 /**
  * The BRDF of an isotropic material is invariant under rotation
  * around the normal. All other materials are called anisotropic.
+ *
+ * @see http://www.fas.harvard.edu/~lib175/projects_fall_2000/vernal/
  */
 class BRDF {
 
