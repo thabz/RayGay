711faf691198018b18f4c0b757aa04aca8a3f224
Jesper Christensen <jesper@kalliope.org>
2004-07-29 08:08:21 +0000

    Docs

--- src/renderersettings.h
+++ src/renderersettings.h
@@ -9,6 +9,7 @@ class RendererSettings {
     public:
 	RendererSettings();
 
+	/// The type of renderer
 	enum RendererType {
 	    PHOTON_RENDERER,
 	    RAYTRACER,
@@ -16,14 +17,23 @@ class RendererSettings {
 	    NONE 
 	};
 
+	/// The maximum number of global photons to store
 	int global_photons_num;
+	/// The maximum number of caustic photons to store
 	int caustic_photons_num;
+	/// The estimate radius when doing an irrandiance estimate from the photonmaps
 	int estimate_radius;
+	/// The maximum number of photons to consider when doing an irrandiance estimate from the photonmaps
 	int estimate_samples;
+	/// The number of rays to shoot when doing final gathering
 	int final_gather_rays;
+	/// The number of paths to shoot through each image pixel when doing path tracing
 	int camera_paths;
+	/// The tolerance in the photonmap cache
 	double cache_tolerance;
+	/// The type of renderer to use
 	RendererType renderertype;
+	/// The number of threads to spawn
 	int threads_num;
 };
 
