b29fa976b4f4448f182f01a55332d078376ca108
Jesper Christensen <jesper@kalliope.org>
2004-07-31 14:25:35 +0000

    New testfile

---
+++ test/testparser.cpp
@@ -0,0 +1,18 @@
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "parser/parserclass.h"
+#include <cstdlib>
+#include <cassert>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+ //   Parser p = Parser("mm");
+
+
+    return EXIT_SUCCESS;
+}
