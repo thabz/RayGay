75ca81c8e572ae5b41f509f41e2ad2c04401fd4c
Jesper Christensen <jesper@kalliope.org>
2004-08-01 11:27:29 +0000

    More tests

--- test/testparser.cpp
+++ test/testparser.cpp
@@ -6,12 +6,14 @@
 #include "parser/parserclass.h"
 #include "parser/assignments.h"
 #include <cstdlib>
+#include <cmath>
 #include <cassert>
 #include <iostream>
 
 using namespace std;
 
 #define lookup_float(s) Assignments::getUniqueInstance()->getNamedFloat(s)
+#define lookup_vector(s) Assignments::getUniqueInstance()->getNamedVector(s)
 #define assert_equal(a,b) assert(IS_EQUAL((a),(b)))
 
 void test_float_ops() {
@@ -26,7 +28,17 @@ void test_float_ops() {
     assert_equal(lookup_float("c"),3);
 }
 
+void test_vector_ops() {
+    Parser p = Parser("scenes/test_vector_ops.gay");
+    p.parse();
+    p.execute();
+    assert(lookup_vector("a") == Vector(1,1,1));
+    assert(lookup_vector("b") == Vector(1,2,3));
+    assert_equal(lookup_float("c"),sqrt(1.0 + 4.0 + 9.0));
+}
+
 int main(int argc, char *argv[]) {
+    test_vector_ops();
     test_float_ops();
     return EXIT_SUCCESS;
 }
