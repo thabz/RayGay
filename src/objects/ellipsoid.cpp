c07bec8369b8c4e9364dc857388ec38432e404c1
Jesper Christensen <jesper@kalliope.org>
2004-09-06 21:29:24 +0000

    Avoid using normalize() in full intersect.

--- src/objects/ellipsoid.cpp
+++ src/objects/ellipsoid.cpp
@@ -3,9 +3,11 @@
 #include "boundingbox.h"
 
 Ellipsoid::Ellipsoid(const Vector& center, const Vector& radii, Material* material) : Solid(material) {
+    // The ellipsoid in object space is simply a unit-sphere
+    // with its center at the origin.
     sphere = new Sphere(Vector(0,0,0),1,NULL);
-    this->transform(Matrix::matrixScale(radii));
-    this->transform(Matrix::matrixTranslate(center));
+    transform(Matrix::matrixScale(radii));
+    transform(Matrix::matrixTranslate(center));
 }
 
 void Ellipsoid::transform(const Matrix& m) {
@@ -16,9 +18,13 @@ BoundingBox Ellipsoid::boundingBoundingBox() const {
     return bboxToWorld(sphere->boundingBoundingBox());
 }
 
-Intersection Ellipsoid::_fullIntersect(const Ray& world_ray, const double t) const {
+Intersection Ellipsoid::_fullIntersect( const Ray& world_ray, const double t) const {
     Ray ray = rayToObject(world_ray);
-    Intersection local_i = sphere->fullIntersect(ray,t*ray.t_scale);
+    double new_t = t*ray.t_scale;
+    Vector p = ray.getPoint(new_t);
+    // The normalized normal at a surface point of a unit-sphere
+    // is the same as the surface point itself.
+    Intersection local_i = Intersection(p,new_t,p,Vector2(0,0));
     return intersectionToWorld(local_i);
 }
 
@@ -26,7 +32,6 @@ double Ellipsoid::_fastIntersect(const Ray& world_ray) const {
     Ray local_ray = rayToObject(world_ray);
     double res = sphere->fastIntersect(local_ray);
     return res / local_ray.t_scale;
-    return (res > 0) ? res / local_ray.t_scale : -1;
 }
 
 SceneObject* Ellipsoid::clone() const {
