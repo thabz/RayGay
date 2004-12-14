63f1549ca339f5d29defd0598469588484a4ff52
Jesper Christensen <jesper@kalliope.org>
2004-12-14 18:46:57 +0000

    Survive a smaller EPSILON

--- src/objects/triangle.cpp
+++ src/objects/triangle.cpp
@@ -130,7 +130,7 @@ double Triangle::_fastIntersect(const Ray& ray) const {
       /* calculate U parameter and test bounds */
       u = DOT(tvec,pvec);
 
-      if (u < 0.0 || u > det)
+      if (u < EPSILON || u > det)
 	 return -1.0;
       
       /* prepare to test V parameter */
@@ -138,7 +138,7 @@ double Triangle::_fastIntersect(const Ray& ray) const {
       
       /* calculate V parameter and test bounds */
       v = DOT(ray.getDirection(),qvec);
-      if (v < 0.0 || u + v > det)
+      if (v < EPSILON || u + v > det)
 	 return -1.0;
    }
 #if 0 
@@ -177,7 +177,7 @@ BoundingBox Triangle::boundingBoundingBox() const {
     tri[1] = mesh->cornerAt(vertex[1]);
     tri[2] = mesh->cornerAt(vertex[2]);
     BoundingBox b = BoundingBox::enclosure(tri,3);
-    b.grow(20.0 * EPSILON);
+    b.grow(100000.0 * EPSILON);
     return b;
 }
 
