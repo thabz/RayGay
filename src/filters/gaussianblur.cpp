74c63d6c572d9f6702743b6ab1c6e641dc17543e
Jesper Christensen <jesper@kalliope.org>
2004-12-22 16:00:27 +0000

    Use alloca for mask instead of new

--- src/filters/gaussianblur.cpp
+++ src/filters/gaussianblur.cpp
@@ -11,12 +11,9 @@ GaussianBlur::GaussianBlur(double radius) {
 }
 
 void GaussianBlur::apply(Image* image) {
-    //(2PI)-.5exp(-u2/2). Here u=(x-xi)/h,
-
-    // Build gaussian kernel
 
     int h = (int)radius;
-    double* mask = new double[h*h];
+    double* mask = (double*) alloca( sizeof(double) * h * h);
 
     for(int x = 0; x < radius; x++) {
 	for(int y = 0; y < radius; y++) {
@@ -30,5 +27,4 @@ void GaussianBlur::apply(Image* image) {
     normalizeMask(mask,h,h);
 
     applyMask(image, mask, h, h);
-    delete mask;
 }
