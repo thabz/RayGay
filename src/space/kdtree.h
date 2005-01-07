65b2150499125abc5591114eeb1a3fa7bd7ebc10
Jesper Christensen <jesper@kalliope.org>
2005-01-07 14:38:22 +0000

    Use real restrict

--- src/space/kdtree.h
+++ src/space/kdtree.h
@@ -2,6 +2,10 @@
 #ifndef SPACE_KD_TREE_H
 #define SPACE_KD_TREE_H
 
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #include "types.h"
 #include "math/vector.h"
 #include "boundingbox.h"
@@ -47,7 +51,7 @@ class KdTree {
 		// Left child when not a leaf. Right child is left + 1.
 		uint left;  
 		// Enclosed objects when this is a leaf
-		Object** __restrict__ objects;
+		Object** restrict objects;
 	    };
 	    union {
 		// Number of objects when this is a leaf
@@ -60,10 +64,10 @@ class KdTree {
 	};
 
 	struct StackElem {
-	    const KdNode* __restrict__ node;   // pointer to far child
+	    const KdNode* restrict node;   // pointer to far child
 	    float t;        // the entry/exit signed distance
 	    int prev;       // pointer to previus stack item
-	    double __restrict__ pb[3];   // coordinates of entry/exit point
+	    double restrict pb[3];   // coordinates of entry/exit point
 	};
 
 	// The I/O data for the findBestSplitPlane method
@@ -92,8 +96,8 @@ class KdTree {
 
 	std::vector<Object*>* added_objects;
 
-	BoundedObject** __restrict__ left_bobs;
-	BoundedObject** __restrict__ right_bobs;
+	BoundedObject** restrict left_bobs;
+	BoundedObject** restrict right_bobs;
 };
 
 
