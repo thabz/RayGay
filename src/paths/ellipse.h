d6ec6b2baa5388fab032b8c2267184978c2f17ad
Jesper Christensen <jesper@kalliope.org>
2005-01-27 09:07:56 +0000

    Merged ellipse from scheme branch

---
+++ src/paths/ellipse.h
@@ -0,0 +1,29 @@
+
+#ifndef PATHS_ELLIPSE_H
+#define PATHS_ELLIPSE_H
+
+#include "paths/path.h"
+#include "math/vector.h"
+#include "math/matrix.h"
+
+/// An ellipse path
+class Ellipse : public Path {
+
+    public:
+	Ellipse(const Vector& center, double r1, double r2, const Vector& normal);
+	virtual ~Ellipse() {};
+	Vector getPoint(double t) const;
+	Vector getTangent(double t) const;
+        void transform(const Matrix& m);
+
+    private:
+	Vector c;
+	Vector n;
+	double r1;
+	double r2;
+	Matrix m;
+	Matrix orient;
+};
+
+
+#endif
