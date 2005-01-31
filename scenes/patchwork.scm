d1d33efdb4b74d09a03fe3c05ec691d014dc6f84
Jesper Christensen <jesper@kalliope.org>
2005-01-31 15:24:05 +0000

    New scene

---
+++ scenes/patchwork.scm
@@ -0,0 +1,73 @@
+
+(load "globals.scm")
+(load "objects.scm")
+
+(set! image-size '(1024 1024))
+(set! background '(0.3 0.6 0.7))
+
+(set! renderer "raytracer")
+(set! camera 
+  (make-pinhole-camera 
+    '( pos (10 1000 3000)
+       lookat (0 0 0)
+       up (0 1 0)
+       fov 45
+       aa 4)))
+
+(define brown
+  (make-material
+    '( diffuse (0.7 0.4 0.2)
+       kd 1.0
+       ks 0.0)))
+
+(define grey85
+  (make-material
+    '( diffuse (0.85 0.85 0.85)
+       kd 1.0
+       ks 0.0)))
+
+(define chrome
+  (make-material
+    '( diffuse (0.8 0.8 0.8)
+       kd 0.2
+       specular (1.0 1.0 1.0)
+       ks 0.8
+       specpow 30)))
+
+
+(set! scene (list (make-pointlight '(500 1300 1300))))
+;(append! scene (list (make-pointlight '(-500 1500 1300))))
+
+(define img 
+ (make-texture 
+  "gfx/larry.jpg" 1.0 1.0 "none"))
+
+(define num 48)
+
+(let itery ((y (* -1 num)))
+  (if (not(= y num))
+    (begin 
+      (let iterx ((x (* -1 num)))
+	(if (not(= x num))
+	  (begin
+	    (append! 
+	      scene
+	      (list 
+		(make-sphere
+		  (list (* x 20) (* y -20) 0) 15.0
+		  (make-material 
+		    (list 
+		      'diffuse 
+		      (get-pixel 
+			img 
+			(/ (+ x num) (* 2 num)) 
+			(/ (+ y num) (* 2 num)))
+		      'kd 0.8
+		      'ks 0.0)))))
+	    (iterx (+ x 1)))))
+      (itery (+ y 1)))))
+
+
+
+(display "End of patch.work")
+(newline)
