5792e34e70153ec8b7623800c4fcb9d38b113062
Jesper Christensen <jesper@kalliope.org>
2005-01-31 09:59:12 +0000

    New file

---
+++ src/paths/superellipse.h
@@ -0,0 +1,25 @@
+
+#ifndef PATH_SUPERELLIPSE_H
+#define PATH_SUPERELLIPSE_H
+
+/**
+ * The superellipse path. The curve has the Cartesian equation
+ *
+ * \f[ \left | \frac{x}{a} \right |^r + \left | \frac{y}{b} \right |^r = 1 \f]
+ * 
+ * The curve is described parametrically as
+ *
+ * \f[ F(t) = ( a\cos^{2/r}t, b\sin^{2/r}t ) \f]
+ *
+ * It's tangent vector becomes
+ *
+ * \f[ F'(t) = ( -\frac{2a}{r} \cos^{2/r - 1}t \sin t, \frac{2b}{r} \sin^{2/r - 1}t \cos t \f]
+ *
+ * @see http://mathworld.wolfram.com/Superellipse.html
+ */
+class SuperEllipse : public Path {
+
+
+};
+
+#endif
