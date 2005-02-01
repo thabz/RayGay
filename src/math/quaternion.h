c4ef1de12ebd3f94e51c15119efeaa5cc7f1ff3a
Jesper Christensen <jesper@kalliope.org>
2005-02-01 12:36:29 +0000

    More

--- src/math/quaternion.h
+++ src/math/quaternion.h
@@ -17,14 +17,21 @@ class Quaternion
     friend std::ostream& operator<<(std::ostream &os, const Quaternion& q);
 
     public:
+   	/// Default constructor
         Quaternion();
+	/// Constructor
 	Quaternion(double a, double b, double c, double d);
+	/// Constructor
 	Quaternion(double s, const Vector& v);
 	Quaternion operator+(const Quaternion& b) const;
 	Quaternion operator*(const Quaternion& b) const;
+	/// Scale quaternion
 	Quaternion operator*(double b) const;
+	/// Scale quaternion
 	Quaternion operator/(double b) const;
+	/// Test for equality
 	bool operator==(const Quaternion& b) const;
+	/// Test for inequality
 	bool operator!=(const Quaternion& b) const;
 	Quaternion conjugate() const;
 	Quaternion inverse() const;
@@ -59,6 +66,9 @@ Quaternion::Quaternion(double s, const Vector& v) {
     a4 = v[2];
 }
 
+/**
+ * Add two quaternions.
+ */
 Quaternion Quaternion::operator+(const Quaternion& b) const {
     return Quaternion(a1 + b.a1, a2 + b.a2, a3 + b.a3, a4 + b.a4);
 }
@@ -174,8 +184,14 @@ Vector Quaternion::rotate(const Vector& P) const {
 }
 
 /**
- * Converts this quaternion to a rotation matrix. The quaternion must
- * have norm = 1.
+ * Converts this quaternion to the corresponding orthogonal rotation matrix. 
+ * The quaternion must have norm = 1. The resulting matrix for a 
+ * quaternion \f$ q = (a,b,c,d) \f$ when \f$ |q| = 1 \f$ is 
+ *
+ * \f[ \left[ \begin{array}{ccc}
+  a^2 + b^2 - c^2 - d^2 & 2bc - 2ad & 2ac + 2bd \\
+  2ad + 2bc & a^2 - b^2 + c^2 - d^2 & 2cd - 2ab \\
+  2bd - 2ac & 2ab + 2cd & a^2 - b^2 - c^2 + d^2 \end{array} \right] \f]
  *
  * @see http://en.wikipedia.org/wiki/Quaternions_and_spatial_rotation
  */
@@ -186,18 +202,18 @@ Matrix Quaternion::toMatrix() const {
     double b = a2;
     double c = a3;
     double d = a4;
-    double aa = a * a;
-    double bb = b * b;
-    double cc = c * c;
-    double dd = d * d;
+    double aa = a * a; double bb = b * b; double cc = c * c; double dd = d * d;
+    double b2 = 2*b; double d2 = 2*d;
+    double bc2 = b2*c; double ad2 = d2*a; double ac2 = 2*a*c;
+    double 2bd = b2*d; double cd2 = d2*c; double ab2 = b2*a;
     m.set(0,0, aa + bb - cc - dd);
-    m.set(1,0, 2*b*c - 2*a*d);
-    m.set(2,0, 2*a*c - 2*b*d);
-    m.set(0,1, 2*a*d + 2*b*c);
+    m.set(1,0, bc2 - ad2);
+    m.set(2,0, ac2 - bd2);
+    m.set(0,1, ad2 + bc2);
     m.set(1,1, aa - bb + cc - dd);
-    m.set(2,1, 2*c*d - 2*a*b);
-    m.set(0,2, 2*b*d - 2*a*c);
-    m.set(1,2, 2*a*b + 2*c*d);
+    m.set(2,1, cd2 - ab2);
+    m.set(0,2, bd2 - ac2);
+    m.set(1,2, ab2 + cd2);
     m.set(2,2, aa - bb - cc + dd);
     return m;
 }
