fabf54e7eea0fe68a008a96978ef2f467de4bcdf
Jesper Christensen <jesper@kalliope.org>
2005-02-12 20:03:35 +0000

    Better text

--- scenes/lib/l-system.scm
+++ scenes/lib/l-system.scm
@@ -1,14 +1,14 @@
 ;; Performs a L-system expansion given an axiom, a set of rules
-;; and a recursion depth.
+;; and a wanted generation.
 ;;
 ;; EXAMPLE L-SYSTEM
 ;;    Axiom: a b c
 ;;    Rules: a -> a b
 ;;           b -> b a
 ;; RESULT
-;;    Depth 1: a b b a c
-;;    Depth 2: a b b a b a a b c
-;;    Depth 3: a b b a b a a b b a a b a b b a c 
+;;    Generation 1: a b b a c
+;;    Generation 2: a b b a b a a b c
+;;    Generation 3: a b b a b a a b b a a b a b b a c 
 ;;
 ;; The above rules should be formulated in Scheme as an association list:
 ;;
@@ -16,12 +16,12 @@
 ;;   '((a . (a b))
 ;;    (b . (b a))))
 ;;
-;; The the expansion of the axiom (a b c) at depth 3 is done as
+;; The the expansion of the axiom (a b c) at generation 3 is done as
 ;; 
 ;; (l-system-expand '(a b c) rules 3)
 ;; 
-(define (l-system-expand axiom rules depth)
-  (if (zero? depth) 
+(define (l-system-expand axiom rules generation)
+  (if (zero? generation) 
     axiom
     (l-system-expand 
       (let iter 
@@ -34,4 +34,4 @@
 	  (if (not (null? rest))
 	    (iter (car rest) (cdr rest))
 	    '())))
-      rules (- depth 1))))
+      rules (- generation 1))))
