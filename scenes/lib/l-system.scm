ccd4919a127475d7a0a7e39daac720fa888aed5f
Jesper Christensen <jesper@kalliope.org>
2005-02-12 19:58:09 +0000

    Done.

--- scenes/lib/l-system.scm
+++ scenes/lib/l-system.scm
@@ -1,5 +1,26 @@
-
-(define (l-system-expand axiom depth)
+;; Performs a L-system expansion given an axiom, a set of rules
+;; and a recursion depth.
+;;
+;; EXAMPLE L-SYSTEM
+;;    Axiom: a b c
+;;    Rules: a -> a b
+;;           b -> b a
+;; RESULT
+;;    Depth 1: a b b a c
+;;    Depth 2: a b b a b a a b c
+;;    Depth 3: a b b a b a a b b a a b a b b a c 
+;;
+;; The above rules should be formulated in Scheme as an association list:
+;;
+;; (define rules
+;;   '((a . (a b))
+;;    (b . (b a))))
+;;
+;; The the expansion of the axiom (a b c) at depth 3 is done as
+;; 
+;; (l-system-expand '(a b c) rules 3)
+;; 
+(define (l-system-expand axiom rules depth)
   (if (zero? depth) 
     axiom
     (l-system-expand 
@@ -7,11 +28,10 @@
 	((item (car axiom))
 	 (rest (cdr axiom)))
 	(append 
-	  (cond
-	    ((eq? item 'a) '(a b))
-	    ((eq? item 'b) '(b x a b x c))
-	    (else (list item)))
+	  (if (assq-ref rules item)
+	    (assq-ref rules item)
+	    (list item))
 	  (if (not (null? rest))
 	    (iter (car rest) (cdr rest))
 	    '())))
-      (- depth 1))))
+      rules (- depth 1))))
