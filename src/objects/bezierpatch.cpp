338aa3a20d7f11906f3b9c7d5f5014c48f2c42db
Jesper Christensen <jesper@kalliope.org>
2005-04-07 07:52:19 +0000

    Inherit from parametrized surface to leverage quad-generator.

--- src/objects/bezierpatch.cpp
+++ src/objects/bezierpatch.cpp
@@ -10,34 +10,18 @@
  * @param yResolution Number of quads down
  * @param material The material to use
  */
-BezierPatch::BezierPatch(const vector<Vector> &points, const uint xResolution, const uint yResolution, const Material* material) : Mesh(Mesh::MESH_PHONG,material) {
- //   this->controlPoints = new Vector[16];
+BezierPatch::BezierPatch(
+	const vector<Vector> &points, 
+	const uint xResolution, 
+	const uint yResolution, 
+	const Material* material) : ParametrizedSurface(xResolution, yResolution, false, false, material) {
     for(uint i = 0; i < 16; i++) {
 	controlPoints[i] = points[i];
-    }
-    Vector2 uvs[4];
-    Vector verts[4];
-
-    for(uint x = 0; x < xResolution; x++) {
-	for(uint y = 0; y < yResolution; y++) {
-	    double u1 = double(x) / double(xResolution);
-	    double u2 = double(x+1) / double(xResolution);
-	    double v1 = double(y) / double(yResolution);
-	    double v2 = double(y+1) / double(yResolution);
-	    uvs[0] = Vector2(u1,v1);
-	    uvs[1] = Vector2(u2,v1);
-	    uvs[2] = Vector2(u2,v2);
-	    uvs[3] = Vector2(u1,v2);
-	    for(int i = 0; i < 4; i++) {
-		verts[i] = getPoint(uvs[i]);
-	    }
-	    addQuad(verts,uvs);
-	}
     }
 }
 
 const Vector& BezierPatch::getControlPoint(uint i, uint j) const {
-    return controlPoints[i + j * 4];
+    return controlPoints[i * 4 + j];
 }
 
 double B(uint n, double u) {
@@ -56,9 +40,7 @@ double B(uint n, double u) {
     }
 }
 
-Vector BezierPatch::getPoint(const Vector2& c) const {
-    const double u = c[0];
-    const double v = c[1];
+Vector BezierPatch::eval(double u, double v) const {
     Vector result = Vector(0,0,0);
     for (uint i = 0; i < 4; i++) {
 	for (uint j = 0; j < 4; j++) {
--- src/objects/bezierpatch.h
+++ src/objects/bezierpatch.h
@@ -2,20 +2,22 @@
 #ifndef BEZIER_PATCH
 #define BEZIER_PATCH
 
-#include "mesh.h"
+#include "parametrizedsurface.h"
 #include <vector>
 using namespace std;
 
 /**
  * A bicubic bezier patch.
  */
-class BezierPatch : public Mesh {
+class BezierPatch : public ParametrizedSurface {
     public:
 	BezierPatch(const vector<Vector> &points, const unsigned int xResolution, unsigned int yResolution, const Material* material);
 
+    protected:
+	Vector eval(double u, double v) const;
+
     private:
 	const Vector& getControlPoint(unsigned int i, unsigned int j) const;
-	Vector getPoint(const Vector2& c) const;
 	Vector controlPoints[16];
 };
 
