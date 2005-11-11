f4e0aa46898ac235aec1a1fde30e6e60a521fbf2
Jesper Christensen <jesper@kalliope.org>
2005-11-11 13:38:33 +0000

    Fix for gcc 4.0

--- src/types.h
+++ src/types.h
@@ -2,9 +2,17 @@
 #ifndef RAYGAY_TYPES_H
 #define RAYGAY_TYPES_H
 
-#define uint unsigned int
-#define ushort unsigned short
-#define uchar unsigned char
-#define ulong unsigned long
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#ifdef HAVE_SYS_TYPES_H
+    #include <sys/types.h>
+#else
+    #define uint unsigned int
+    #define ushort unsigned short
+    #define uchar unsigned char
+    #define ulong unsigned long
+#endif
 
 #endif
