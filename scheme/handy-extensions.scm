360e7b73025642e82e8de14bc1be7a47c8fb3d49
Jesper Christensen <jesper@kalliope.org>
2006-05-10 19:10:03 +0000

    Using optional materials

--- scheme/handy-extensions.scm
+++ scheme/handy-extensions.scm
@@ -12,3 +12,12 @@
 (define (.x vec) (car vec))
 (define (.y vec) (list-ref vec 1))  
 (define (.z vec) (list-ref vec 2))
+
+(define (rotate-x obj angle)
+ (rotate obj x-axis angle))
+
+(define (rotate-y obj angle)
+ (rotate obj y-axis angle))
+
+(define (rotate-z obj angle)
+ (rotate obj z-axis angle))
