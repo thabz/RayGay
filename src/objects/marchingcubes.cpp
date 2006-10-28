0191665e6cbd20ea0fb1d67aaeb27dba7c1fd05e
Jesper Christensen <jesper@kalliope.org>
2006-10-28 09:49:18 +0000

    Fixed some of the cases where vertices weren't correctly reused.

--- src/objects/marchingcubes.cpp
+++ src/objects/marchingcubes.cpp
@@ -379,14 +379,14 @@ uint32_t cube_index(const vector<bool>& inouts, uint32_t s, uint32_t xi, uint32_
 // This table specifies which two cube vertices are
 // at the end of each of the 12 cube edges.
 static const uint32_t connecting_lines[12][2] =  {
-    {0,1}, {1,2}, {2,3}, {3,0},
-    {4,5}, {5,6}, {6,7}, {7,4}, 
+    {0,1}, {1,2}, {3,2}, {0,3},
+    {4,5}, {5,6}, {7,6}, {4,7}, 
     {0,4}, {1,5}, {2,6}, {3,7}
 };
 
 static const uint32_t offsets[12][4] = {
-    {0,0,0,0}, {1,0,0,1}, {1,0,1,0}, {0,0,1,1},
-    {0,1,0,0}, {1,1,0,1}, {1,1,1,0}, {0,1,1,1},
+    {0,0,0,0}, {1,0,0,1}, {0,0,1,0}, {0,0,0,1},
+    {0,1,0,0}, {1,1,0,1}, {0,1,1,0}, {0,1,0,1},
     {0,0,0,2}, {1,0,0,2}, {1,0,1,2}, {0,0,1,2}        
 };
 
