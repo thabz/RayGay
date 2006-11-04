b7ace82bed5443b732c384fd295a6861200ee726
Jesper Christensen <jesper@kalliope.org>
2006-11-04 10:32:27 +0000

    New files

---
+++ src/color/Lab.h
@@ -0,0 +1,22 @@
+
+#ifndef COLOR_LAB_H
+#define COLOR_LAB_H
+
+#include "color/XYZ.h"
+
+/**
+ * The CIE 1976 L*a*b* color space.
+ *
+ * @see http://en.wikipedia.org/wiki/Lab_color_space
+ */
+class Lab 
+{
+    public:
+	XYZ toXYZ(XYZ whitepoint) const;
+	RGB toRGB(double gamma = 2.2);
+
+    private:
+	double L, a, b;
+};
+
+#endif
---
+++ src/color/XYZ.h
@@ -0,0 +1,18 @@
+
+#ifndef COLOR_XYZ_H
+#define COLOR_XYZ_H
+
+/**
+ * The CIE 1931 XYZ color space
+ *
+ * @see http://en.wikipedia.org/wiki/CIE_1931_color_space
+ */
+class XYZ {
+    public:
+	Lab toLab(XYZ whitepoint) const;
+
+    private:
+	double X,Y,Z;
+};
+
+#endif
