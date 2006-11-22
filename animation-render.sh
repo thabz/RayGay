818ba982faf1fa5ce017a40894f90e1ee45d4545
Jesper Christensen <jesper@kalliope.org>
2006-11-22 12:40:01 +0000

    Bail out if one frame fails to render

--- animation-render.sh
+++ animation-render.sh
@@ -23,6 +23,11 @@ do
    OUTPUT_FILE=output`printf %05d $i`.png
    echo Rendering $SCENE_FILE frame $i of $FRAMES_NUM to $OUTPUT_FILE
    ./src/tracer -b -f $i -F $FRAMES_NUM $SCENE_FILE $OUTPUT_FILE
+   if [ $? -ne 0 ]; then
+      exit $?
+   fi
    i=`expr $i + 1`
 done
+
+exit 0
 
