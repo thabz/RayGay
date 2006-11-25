e22be0eaccb6581cb55f1ca8a6b65388b520e9c2
Jesper Christensen <jesper@kalliope.org>
2006-11-25 16:24:15 +0000

    More accurate

--- src/objects/tetrahedron.cpp
+++ src/objects/tetrahedron.cpp
@@ -3,7 +3,7 @@
 
 Tetrahedron::Tetrahedron(const Vector a, const double r, const Material* mat) : Mesh(Mesh::MESH_FLAT,mat) {
 
-    double sqrt_3 = 0.5773502692 * r; // Found on the internet
+    double sqrt_3 = 0.577350269189626 * r; // Found on the internet
     Vector PPP = Vector( sqrt_3,  sqrt_3,  sqrt_3 ) + a; 
     Vector MMP = Vector(-sqrt_3, -sqrt_3,  sqrt_3 ) + a;
     Vector MPM = Vector(-sqrt_3,  sqrt_3, -sqrt_3 ) + a;
