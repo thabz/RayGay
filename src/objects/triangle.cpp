b120c7bd9cd493023bf4e9f9dedce27a73c47fd6
Jesper Christensen <jesper@kalliope.org>
2006-11-25 15:59:38 +0000

    Remove crappy extending boundingbox. Unneeded now the Vector::normalize() problem has been fixed

--- src/objects/triangle.cpp
+++ src/objects/triangle.cpp
@@ -197,12 +197,7 @@ AABox Triangle::getBoundingBox() const {
     tri[0] = mesh->cornerAt(_tri_idx,0);
     tri[1] = mesh->cornerAt(_tri_idx,1);
     tri[2] = mesh->cornerAt(_tri_idx,2);
-    AABox b = AABox::enclosure(tri,3);
-    b.grow(100000.0 * EPSILON);
-    // TODO: Woa! Too much.
-    b.growPercentage(0.1);
-    //b.grow(100000.0 * EPSILON);
-    return b;
+    return AABox::enclosure(tri,3);
 }
 
 double Triangle::area() const {
