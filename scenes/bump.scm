55552ee4e4df90bb42d9251838fff78588bb05ea
Jesper Christensen <jesper@kalliope.org>
2006-11-27 20:00:44 +0000

    Simplify Scheme

--- scenes/bump.scm
+++ scenes/bump.scm
@@ -3,8 +3,8 @@
 (load "lib/objects/make-rounded-box.scm")
 (load "lib/objects/make-rounded-cylinder.scm")
 
-(set-image-size '(1600 1200))
-;(set! image-size '(640 480))
+;(set-image-size '(1600 1200))
+(set-image-size '(640 480))
 ;(set! background (make-texture "gfx/goodmorning.jpg" 1 1 "bilinear"))
 (set-background '(0.76 0.62 0.42))
 
@@ -15,7 +15,7 @@
        lookat (0 0 0)
        up (0 1 0)
        fov 45
-       aa 4)))
+       aa 0)))
 
 (define (perturb-noise P N)
  (let ((pnoise (noise3d (vscale P 5) 0.001)))
@@ -28,13 +28,12 @@
 
 (define chrome
   (make-material
-    (list 'diffuse '(1.0 1.0 1.0)
-	  'kd 0.4
-	  'specular '(1.0 1.0 1.0)
-	  'ks 0.6
-       'specpow 35
-       'normal perturb-noise 
-       )))
+    `(diffuse (1.0 1.0 1.0)
+      kd 0.4
+      specular (1.0 1.0 1.0)
+      ks 0.6
+      specpow 35
+      normal ,perturb-noise)))
 
 (define brown1
  (make-material
@@ -48,54 +47,40 @@
      kd 1.0
      ks 0.0)))
 
-
-
 (add-to-scene (make-arealight '(1300 1300 1300) '(-1 -1 -1) 200 80 0.1))
-(add-to-scene (make-arealight '(1300 1300 1300) '(-1 -1 -1) 200 80 0.1))
+;(add-to-scene (make-arealight '(1300 1300 1300) '(-1 -1 -1) 200 80 0.1))
 
 (add-to-scene
   (list 
-   (make-sphere '(8 2 8) 2 chrome)
-   (translate
-    (make-torus 3 1 chrome)
-    '(2 1 14))
-   (make-sphere '(0 4 0) 4 chrome)))
+    (make-sphere '(8 2 8) 2 chrome)
+    (translate
+      (make-torus 3 1 chrome)
+      '(2 1 14))
+    (make-sphere '(0 4 0) 4 chrome)))
 
 (add-to-scene
  (translate
   (make-rounded-cylinder 5 3 0.1 chrome)
   '(-8 0 9)))
 
-
 (define num 20)
 (define offset 4)
 (define width (- offset 0.02))
 (define radius (/ width 10))
 
-(let loopx
-  ((x 0))
-  (if (< x num)
-    (begin
-      (let loopz
-	((z 0))
-	(if (< z num)
-	  (begin 
-	   (add-to-scene
-		     (translate
-		       (make-rounded-box 
-			 (list (* offset x) 0 (* offset z))
-			 (list (+ width (* offset x)) 
-			       width 
-			       (+ width (* offset z)))
-			 radius
-			 (if (eq? 
-			       (modulo x 2)
-			       (modulo z 2))
-			   brown1
-			   brown2))
-		       (list (* num width -0.5) (- width) (* num width -0.5))))
-	    (loopz (+ 1 z)))))
-      (loopx (+ 1 x)))))
+; Tiled floor
+(dotimes x num
+   (dotimes z num        
+      (add-to-scene
+        (translate
+           (make-rounded-box 
+	     `( ,(* offset x) 0 ,(* offset z))
+	     `( ,(+ width (* offset x)) ,width ,(+ width (* offset z)))
+	     radius
+	     (if (eq? (modulo x 2) (modulo z 2))
+	       brown1
+	       brown2))
+	   `(,(* num width -0.5) ,(- width) ,(* num width -0.5))))))
 
 
 
