a7c325c140f175e4236a4aa7eb0a5212d4681457
Jesper Christensen <jesper@kalliope.org>
2007-06-13 07:03:49 +0000

    New space on the way

---
+++ src/space/bih.h
@@ -0,0 +1,13 @@
+
+#ifndef TRACER_SPACE_BIH_H
+#define TRACER_SPACE_BIH_H
+
+// @see http://xitrace.org/ for an implementation
+// @see http://ompf.org/forum/viewtopic.php?t=159
+
+class BoundingIntervalHierarchy {
+
+
+};
+
+#endif
