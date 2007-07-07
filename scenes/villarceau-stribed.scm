5efdfd85fae9ca14d1a61229d4a150ed32fad6b1
Jesper Christensen <jesper@kalliope.org>
2007-07-07 15:45:47 +0000

    Stads

--- scenes/villarceau-stribed.scm
+++ scenes/villarceau-stribed.scm
@@ -23,7 +23,8 @@
        ks 0.0)))
 
 ;(add-to-scene (make-pointlight #(500 1300 1300)))
-(add-to-scene (make-skylight 10000 512 #(1 1 1)))
+;(add-to-scene (make-skylight 10000 512 #(1 1 1)))
+(add-to-scene (make-arealight #(1000 2000 2000) #(-0.5 -1 -1) 500 64 0.1))
 
 (define (make-villarceau-circle R r dir r-offset amplitude periods period-offset)
    (lambda (t)
@@ -50,28 +51,48 @@
    (set! result (cons (make-cylinder p1 p2 radius mat) result))))
  result))  
 
+; See http://en.wikipedia.org/wiki/HSV_color_space
+(define (hsv->rgb hsv)
+  (let* ((H (vector-ref hsv 0))
+         (S (vector-ref hsv 1))
+         (V (vector-ref hsv 2))
+         (Hi (modulo (floor (/ H 60)) 6))
+         (f (- (/ H 60) Hi))
+         (p (* V (- 1 S)))
+         (q (* V (- 1 (* f S))))
+         (t (* V (- 1 (* (- 1 f) S)))))
+    (case Hi 
+      ((0) (vector V t p))     
+      ((1) (vector q V p))     
+      ((2) (vector p V t))     
+      ((3) (vector p q V))     
+      ((4) (vector t p V))     
+      ((5) (vector V p q)))))
 
-(define num-around 30)
+
+(define num-around 16)
 (define torus-R 500)
 (define torus-r 100)
-(define a 3)
-(define tube-r 3)
+(define a 10)
+(define tube-r 20.5)
 (define sections 500)    
 
-(add-to-scene (make-solid-box #(-700 -1 -700) #(700 0 700) chrome))
+;(add-to-scene (make-solid-box #(-700 -1 -700) #(700 0 700) chrome))
+
 (dotimes i num-around
  (add-to-scene
     (translate 
     (stroke-path
       (make-villarceau-circle torus-R torus-r 1
        (* i (/ TWO-PI num-around)) a num-around HALF-PI)
-      tube-r chrome sections)
-      #(0 120 0))
-    (translate  
-    (stroke-path
-      (make-villarceau-circle torus-R torus-r -1
-       (* i (/ TWO-PI num-around)) a num-around (- HALF-PI))
-      tube-r chrome sections)
+      tube-r
+      (make-material
+        `( diffuse ,(hsv->rgb (vector (* i (/ 360 num-around)) 0.5 1.0))
+           kd 0.6
+           specular #(1.0 1.0 1.0)
+           ks 0.4
+           specpow 30))
+      sections)
       #(0 120 0))))
 
     
