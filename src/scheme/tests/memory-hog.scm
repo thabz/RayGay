41b924b6827bfc5b5ff676387299242ecf56c242
Jesper Christensen <jesper@kalliope.org>
2007-07-10 11:19:04 +0000

    Note

--- src/scheme/tests/memory-hog.scm
+++ src/scheme/tests/memory-hog.scm
@@ -2,7 +2,7 @@
 ; Genbrug loop's frame til de nye variable istedet for at lave en
 ; ny frame hver gang, da loop lambda'en er lokal under let'en.
 
-; Alle built-ins skal ikke returnerer et result, men have et dest
+; Alle built-ins skal ikke returnere et result, men have et dest
 ; arg med. Dermed kan ovenstaaende udnyttes fuldt ud og vi
 ; kan koere med kun 100k allocations og ikke 1M.
 
