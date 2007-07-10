63af8d8684c7f82ce53e67073e477661903def35
Jesper Christensen <jesper@kalliope.org>
2007-07-10 11:10:48 +0000

    Ny mappe

---
+++ src/scheme/tests/memory-hog.scm
@@ -0,0 +1,17 @@
+
+; Genbrug loop's frame til de nye variable istedet for at lave en
+; ny frame hver gang, da loop lambda'en er lokal under let'en.
+
+; Alle built-ins skal ikke returnerer et result, men have et dest
+; arg med. Dermed kan ovenstaaende udnyttes fuldt ud og vi
+; kan koere med kun 100k allocations og ikke 1M.
+
+(display
+(reverse 
+(let loop ((i 0)
+           (l '()))
+    (if (= i 100000)
+        l
+	(loop (+ 1 i) (cons i l))))))
+(newline)    
+ 
