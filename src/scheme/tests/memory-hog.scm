f85be02c80f11f09ee42e7f04127dc7367d2fedb
Jesper Christensen <jesper@kalliope.org>
2007-07-14 13:22:14 +0000

    A timing

--- src/scheme/tests/memory-hog.scm
+++ src/scheme/tests/memory-hog.scm
@@ -5,6 +5,8 @@
 ; Alle built-ins skal ikke returnere et result, men have et dest
 ; arg med. Dermed kan ovenstaaende udnyttes fuldt ud og vi
 ; kan koere med kun 100k allocations og ikke 1M.
+;
+; ./repl tests/memory-hog.scm  42.08s user 0.23s system 98% cpu 42.921 total
 
 (display
 (reverse 
