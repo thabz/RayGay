f60f51bbf7996053228da738a1e085396d0e4d85
Jesper Christensen <jesper@kalliope.org>
2007-10-20 10:29:53 +0000

    spelling

--- src/math/poisson_disc.h
+++ src/math/poisson_disc.h
@@ -16,7 +16,7 @@ class PoissonDiscDistribution
 	 * all are at least a distance of 2r from each other.
 	 * 
 	 * @param result must have room for num Vector2's
-	 * @return the number of points found whis is &leq; num
+	 * @return the number of points found which is &leq; num
 	 */
 	int createSet(double r, uint32_t num, Vector2* result);
 
