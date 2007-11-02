fdcf845bd8bda3b4ccd1e430c58bd1781a644c63
Jesper Christensen <jesper@kalliope.org>
2007-11-02 11:52:13 +0000

    Removed testmath. Testparser using wide chars

--- test/testparser.cpp
+++ test/testparser.cpp
@@ -15,9 +15,9 @@
 
 using namespace std;
 
-#define lookupDouble(s) safe_scm2double(p->lookup(s),0,"")
-#define lookupVector(s) scm2vector(p->lookup(s),"",0)
-#define lookupRGBA(s) scm2rgba(p->lookup(s),"",0)
+#define lookupDouble(s) safe_scm2double(p->lookup(s),0,L"")
+#define lookupVector(s) scm2vector(p->lookup(s),L"",0)
+#define lookupRGBA(s) scm2rgba(p->lookup(s),L"",0)
 
 class test_parser : public Test {
     private:
@@ -27,45 +27,45 @@ class test_parser : public Test {
             Scene* scene = new Scene();        
 	    SceneParser* p = new SceneParser(scene);
 
-            p->assignVariable("test-predefined-a", 30);
-	    p->parse_file(getLoadPrefix() + "/scenes/test.scm");
+            p->assignVariable(L"test-predefined-a", 30);
+	    p->parse_file(SchemeFilenames::toString(getLoadPrefix() + "/scenes/test.scm"));
 
-	    assertTrue(IS_EQUAL(lookupDouble("test-predefined-a"),30));
-	    assertTrue(IS_EQUAL(lookupDouble("test-define-1"),10));
+	    assertTrue(IS_EQUAL(lookupDouble(L"test-predefined-a"),30));
+	    assertTrue(IS_EQUAL(lookupDouble(L"test-define-1"),10));
 
-	    assertTrue(IS_EQUAL(lookupDouble("test-vdot-1"),4 + 10 + 18));
-	    assertTrue(IS_EQUAL(lookupDouble("test-vdot-2"),12 - 5 + 12));
+	    assertTrue(IS_EQUAL(lookupDouble(L"test-vdot-1"),4 + 10 + 18));
+	    assertTrue(IS_EQUAL(lookupDouble(L"test-vdot-2"),12 - 5 + 12));
 
-	    assertTrue(IS_EQUAL(lookupDouble("test-vlength-1"),sqrt(double(1.0+4+9))));
-	    assertTrue(IS_EQUAL(lookupDouble("test-vlength-2"),sqrt(double(16.0+4+4))));
+	    assertTrue(IS_EQUAL(lookupDouble(L"test-vlength-1"),sqrt(double(1.0+4+9))));
+	    assertTrue(IS_EQUAL(lookupDouble(L"test-vlength-2"),sqrt(double(16.0+4+4))));
 
-	    assertTrue(lookupVector("test-vscale-1") == Vector(2,4,6));
-	    assertTrue(lookupVector("test-vscale-2") == Vector(12,-6,6));
+	    assertTrue(lookupVector(L"test-vscale-1") == Vector(2,4,6));
+	    assertTrue(lookupVector(L"test-vscale-2") == Vector(12,-6,6));
 
-	    assertTrue(lookupVector("test-vplus-1") == Vector(3,7,12));
-	    assertTrue(lookupVector("test-vplus-2") == Vector(8,3,8));
+	    assertTrue(lookupVector(L"test-vplus-1") == Vector(3,7,12));
+	    assertTrue(lookupVector(L"test-vplus-2") == Vector(8,3,8));
 
-	    assertTrue(lookupVector("test-vminus-1") == Vector(-1,-3,-6));
-	    assertTrue(lookupVector("test-vminus-2") == Vector(0,-7,-4));
+	    assertTrue(lookupVector(L"test-vminus-1") == Vector(-1,-3,-6));
+	    assertTrue(lookupVector(L"test-vminus-2") == Vector(0,-7,-4));
 
 	    v1 = Vector(1,2,3);
 	    v1.normalize();
-	    assertTrue(lookupVector("test-vnormalize-1") == v1);
+	    assertTrue(lookupVector(L"test-vnormalize-1") == v1);
 	    v1 = Vector(4,-2,2);
 	    v1.normalize();
-	    assertTrue(lookupVector("test-vnormalize-2") == v1);
+	    assertTrue(lookupVector(L"test-vnormalize-2") == v1);
 	    delete p;
 
-	    assertTrue(lookupVector("test-translate-1") == Vector(2,3,4));
-	    assertTrue(lookupVector("test-translate-2") == Vector(1,0,-1));
+	    assertTrue(lookupVector(L"test-translate-1") == Vector(2,3,4));
+	    assertTrue(lookupVector(L"test-translate-2") == Vector(1,0,-1));
 
-	    assertTrue(lookupVector("test-rotate-1") == Vector(0,-1,0));
-	    assertTrue(lookupVector("test-rotate-2") == Vector(1,0,0));
+	    assertTrue(lookupVector(L"test-rotate-1") == Vector(0,-1,0));
+	    assertTrue(lookupVector(L"test-rotate-2") == Vector(1,0,0));
 
 	    // RGBA tests
-	    assertTrue(lookupRGBA("color-white") == RGBA(1.,1,1,1));
-	    assertTrue(lookupRGBA("color-white") == RGB(1.,1,1));
-	    assertTrue(lookupRGBA("color-blue-trans") == RGBA(0,0,1,0.5));
+	    assertTrue(lookupRGBA(L"color-white") == RGBA(1.,1,1,1));
+	    assertTrue(lookupRGBA(L"color-white") == RGB(1.,1,1));
+	    assertTrue(lookupRGBA(L"color-blue-trans") == RGBA(0,0,1,0.5));
 	}
 };
 
@@ -76,7 +76,7 @@ int main(int argc, char *argv[]) {
     try {
         suite.run();
     } catch (scheme_exception e) {
-        cerr << e.toString() << endl;    
+        wcerr << e.toString() << endl;    
     }        
     suite.printStatus();
     if (suite.hasFailures()) {
