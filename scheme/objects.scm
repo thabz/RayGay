6f97932e9afcef6d8467d409731225fa3ff8b230
Jesper Christensen <jesper@kalliope.org>
2008-02-26 15:09:51 +0000

    Be gone

--- scheme/objects.scm
+++
@@ -1,8 +0,0 @@
-
-(load "make-necklace.scm")
-(load "make-rounded-plate.scm")
-(load "make-rounded-cylinder.scm")
-(load "make-rounded-box.scm")
-(load "make-bounding-box.scm")
-(load "make-pill.scm")
-
