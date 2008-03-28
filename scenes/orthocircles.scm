2529daf6f0c57dfc04dcf436eed3d9b8a15c7acd
Jesper Christensen <jesper@kalliope.org>
2008-03-28 16:25:30 +0000

    New file

---
+++ scenes/orthocircles.scm
@@ -0,0 +1,4 @@
+; The orthocircles implicit surface is defined by
+; ((x² + y² – 1)² + z²) * ((y² + z² – 1)² + x²) * ((z² + x² – 1)² + y²) – 0.0375² * (1 + 3 * (x² + y² + z²)) = 0 
+;
+; See http://www.flickr.com/photos/mylaboratory/417717535/in/set-72157603650366417/
