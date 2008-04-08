fc7a43eac11aaccd437f3dc2b0fb6ea581c45183
Jesper Christensen <jesper@kalliope.org>
2008-04-08 13:02:20 +0000

    Make sure it's send as png

--- src/http/imageresponse.cpp
+++ src/http/imageresponse.cpp
@@ -7,7 +7,7 @@ ImageResponse::ImageResponse(Image* image) : HTTPResponse (200, "image/png") {
     char filename[L_tmpnam+1];
     char* ptr;
     ptr = tmpnam(filename);
-    image->save(string(ptr));
+    image->save(string(ptr)+".png");
     FILE* f = ::fopen(ptr,"r");
     this->setBody(f);
 };
