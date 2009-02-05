657fd29dad6f66b0013d1c73d415fd700e70d7d5
Jesper Christensen <jesper@kalliope.org>
2009-02-05 12:54:16 +0000

    More work

--- generate.rb
+++ generate.rb
@@ -1,15 +1,23 @@
 #!/usr/bin/ruby -w
 
-OUTPUT = 'build'
+require 'date'
 
-def make_month(year,month) 
+class Entry
+    attr_accessor :title, :date, :content
+		   
+    def initialize(filename)
+       parts = filename.scan(/\d+/)
+       @date = Date.civil(parts[0].to_i,parts[1].to_i,parts[2].to_i)
+    end
+end
+
+def make_month(date) 
 
 end
 
 entry_filenames = Dir.glob("entries/*.html")
-entry_filenames.each { |e|
-   puts e
+entry_filenames.each { |f|
+    e = Entry.new(f)
+    puts e.date	
 }
-
-
 
