67801ec7a6caa953fa8910dc5e0759c570af1870
Jesper Christensen <jesper@kalliope.org>
2009-04-05 18:37:57 +0000

    More work

--- scenes/lib/mesh.scm
+++ scenes/lib/mesh.scm
@@ -5,10 +5,27 @@
 ;; A vertex is a points in space as 3d vectors. 
 ;; A face is a list of indices into the vertices.
 
+(define (unique items)
+ "Returns the unique (in term of equal?) items"
+ (let ((h (make-hashtable equal-hash equal?)))
+   (do ((items items (cdr items)))
+    ((null? items)  
+     (vector->list (hashtable-keys h)))
+    (hashtable-set! h (car items) #t))))
+
+(define (remove-all to-be-removed target-list)
+  "Remove all items from to-be-removed from target-list"
+  (let loop ((to to-be-removed)
+             (ta target-list))
+   (if (null? to)
+    ta
+    (loop (cdr to) (remove (car to) ta)))))
+; Ovenstående kunne måske skrives (fold-right remove to ta)
+
 (define (optimize-mesh m)
   "Optimizes a mesh by removing unused vertices"
-  (define old->new (make-hashtable eqv-hash eqv?))
-  (define new->old (make-hashtable eqv-hash eqv?))
+  (define old->new (make-eqv-hashtable))
+  (define new->old (make-eqv-hashtable))
   (define faces (cadr m))
   (define vertices (car m))
   
@@ -19,7 +36,7 @@
 		    (face (car faces)))
      (if (null? face)
        (insert-loop seq (cdr faces))
-       (if (not (hashtable-ref old->new (car face)))
+       (if (not (hashtable-contains? old->new (car face)))
 	 (begin
           (hashtable-set! old->new (car face) seq)
           (hashtable-set! new->old seq (car face))
@@ -27,17 +44,15 @@
          (face-loop seq (cdr face)))))))
   
   (define new-vertices 
-    (map (lambda (i)
-	   (list-ref vertices 
-	    (hashtable-ref new->old i))) 
-         (hashtable-values old->new)))  
+    (map (lambda (i) (list-ref vertices i))
+         (vector->list (hashtable-keys old->new))))  
 
   (define new-faces
     (map (lambda (face)
 	  (map (lambda (old-index)
-		(hashtable-ref old->new old-index)) 
-	       face))) 
-         old-faces)
+		(hashtable-ref old->new old-index #f)) 
+	       face)) 
+         faces))
 
   ; Return the new mesh
   (list new-vertices new-faces))
@@ -45,6 +60,28 @@
 (define (collapse-nearby-vertices mesh ε)
   "Optimizes a mesh collapsing vertices that are closer than ε"
   ) 
+
+(define (face-find-edges face)
+ "Returns a list of pair representing edges. "
+ "Ie. turns (a b c) into ((a b) (b c) (c a))"
+ (let loop ((f face)
+  	     (r '()))
+  (if (null? (cdr f))
+   (reverse (cons (list (car f) (car face)) r))
+   (loop (cdr f) (cons (list (car f) (cadr f)) r)))))
+
+(define (mesh-extract-edges mesh)
+  "Returns a list of two-item lists" 
+  (let faceloop ((faces (cadr mesh))
+                 (result '()))
+   (if (null? faces)
+    (unique result)
+    (let edgeloop ((edges (face-find-edges (car faces)))
+                   (result result))
+     (if (null? edges)
+      (faceloop (cdr faces) result)
+      (edgeloop (cdr edges)
+                 (cons (list-sort < (car edges)) result)))))))
 
 (define (facing? v1 v2 v3 p)
  "Is the triangle with vertices t1, t2, t3 (clockwise) facing p?"
@@ -56,30 +93,21 @@
 ; Using the 3D Incremental Convex Hull algorithm.
 (define (convex-hull points)
  "Returns a mesh that is the convex hull of a group of points."
- 
- (define (find-edges face)
-  "Returns a list of pair representing edges. "
-  "Ie. turns (a b c) into ((a b) (b c) (c a))"
-  (let loop ((f face)
-   	     (r '()))
-   (if (null? (cdr f))
-    (cons (list (car f) (car face)) r)
-    (loop (cdr f) (list (car f) (cadr f))))))
 
  (define (find-border-edges faces)
   "From a group of connected faces return those edges that "
   "only connects to one face."
   (let face-loop ((faces faces)
-		  (hash (make-hashtable equal?)))
+		  (hash (make-hashtable equal-hash equal?)))
    (if (null? faces)
-    (hashtable-keys hash)
-    (let edge-loop ((edges (find-edges (car faces))))
+    (vector->list (hashtable-keys hash))
+    (let edge-loop ((edges (face-find-edges (car faces))))
      (if (null? edges)
       (face-loop (cdr faces) hash)
       (begin
-       (if (hashtable-ref hash (reverse (car edges)))
+       (if (hashtable-contains? hash (reverse (car edges)))
 	(hashtable-delete! hash (reverse (car edges)))
- 	(hashtable-put hash (car edges)))
+ 	(hashtable-set! hash (car edges) #t))
        (edge-loop (cdr edges))))))))
 
  (define (find-facing-faces hull p)
@@ -101,7 +129,8 @@
  	 (new-index (length (car hull)))
 	 (vertices (append (car hull) (list p)))
          ; Remove facing-faces from hull's faces
-	 (faces (fold-right remove (cadr hull) facing-faces))
+	 ;(faces (fold-left remove (cadr hull) facing-faces))
+	 (faces (remove-all facing-faces (cadr hull)))
 	 (border-edges (find-border-edges facing-faces)))
    (if (null? facing-faces)
     hull ; p is inside hull so return original hull.
@@ -115,10 +144,11 @@
 	          faces))))))) 
 
  (define initial-hull
-  (list (car points) (cadr points) (caddr points) (cadddr points))
-  (list '(0 3 1) '(0 1 2) '(1 3 2) '(3 0 2)))
+  (list
+   (list (car points) (cadr points) (caddr points) (cadddr points))
+   (list '(0 3 1) '(0 1 2) '(1 3 2) '(3 0 2))))
 
  (if (< (length points) 3)
   '()
-  (optimize-mesh (fold-right add-point initial-hull points))))
+  (optimize-mesh (fold-left add-point initial-hull (cddddr points)))))
 
