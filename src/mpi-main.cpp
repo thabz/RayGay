508b5328052709b0f7a1cdd2468ad5698262b3b7
Jesper Christensen <jesper@kalliope.org>
2009-07-19 19:19:49 +0000

    Using C++ API. Sketched the program structure. And it's working on multiple hosts.

--- src/mpi-main.cpp
+++ src/mpi-main.cpp
@@ -1,72 +1,71 @@
 #include <mpi.h>
-#include <stdio.h>
+#include <cstdio>
+#include <cstdlib>
 #include <string.h>
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
  
 #define BUFSIZE 128
 #define TAG 0
 
-enum {
+enum tags {
     READY_FOR_WORK,
     RETURNING_WORK,
     HERE_IS_SOME_WORK,
-} tags;
+};
 
-int master() {
-
-}
-
-int slave() {
+void master() {
     char buff[BUFSIZE];
     bool done = false;
+    MPI::Status stat; 
     while(!done) {
-        MPI_Send(NULL, 0, MPI_CHAR, 0, READY_FOR_WORK, MPI_COMM_WORLD);
-        MPI_Recv(buff, 1024, MPI_CHAR, 0, HERE_IS_SOME_WORK, MPI_COMM_WORLD, &stat);
-        MPI_Send(NULL, 0, MPI_CHAR, 0, RETURNING_WORK, MPI_COMM_WORLD);
+	MPI::COMM_WORLD.Recv(buff, 1024, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, stat);
+	int tag = stat.Get_tag();
+	if (tag == READY_FOR_WORK) {
+	    int source = stat.Get_source();
+	    cout << "Source " << source << " ready for work." << endl;
+	    MPI::COMM_WORLD.Send(NULL, 0, MPI_CHAR, source, HERE_IS_SOME_WORK);
+	} else if (tag == RETURNING_WORK) {
+	    int source = stat.Get_source();
+	    cout << "Source " << source << " returned some work." << endl;
+	}
+    }
+}
+
+void slave(int myid) {
+    char buff[BUFSIZE];
+    bool done = false;
+    MPI_Status stat; 
+    srandom((unsigned int)&stat);
+    while(!done) {
+        MPI::COMM_WORLD.Send(NULL, 0, MPI_CHAR, 0, READY_FOR_WORK);
+	MPI::COMM_WORLD.Recv(buff, 1024, MPI_CHAR, 0, HERE_IS_SOME_WORK);
+	int secs = random() & 7;
+	cout << " Slave " << myid << " got some work for " << secs << " seconds." << endl;
+	sleep(secs);
+        MPI::COMM_WORLD.Send(NULL, 0, MPI_CHAR, 0, RETURNING_WORK);
     }
 }
  
 int main(int argc, char *argv[])
 {
-   char idstr[32];
-   char buff[BUFSIZE];
-   int numprocs;
-   int myid;
-   int i;
-   MPI_Status stat; 
- 
-   MPI_Init(&argc,&argv); /* all MPI programs start with MPI_Init; all 'N' processes exist thereafter */
-   MPI_Comm_size(MPI_COMM_WORLD,&numprocs); /* find out how big the SPMD world is */
-   MPI_Comm_rank(MPI_COMM_WORLD,&myid); /* and this processes' rank is */
+   MPI::Init(argc,argv); /* all MPI programs start with MPI_Init; all 'N' processes exist thereafter */
+   int numprocs = MPI::COMM_WORLD.Get_size(); /* find out how big the SPMD world is */
+   int myid = MPI::COMM_WORLD.Get_rank(); /* and this processes' rank is */
  
    /* At this point, all the programs are running equivalently, the rank is used to
       distinguish the roles of the programs in the SPMD model, with rank 0 often used
       specially... */
-   if(myid == 0)
-   {
-     printf("%d: We have %d processors\n", myid, numprocs);
-     for(i=1;i<numprocs;i++)
-     {
-       sprintf(buff, "Hello %d!\n", i);
-       MPI_Send(buff, BUFSIZE, MPI_CHAR, i, TAG, MPI_COMM_WORLD);
-     }
-     for(i=1;i<numprocs;i++)
-     {
-       MPI_Recv(buff, BUFSIZE, MPI_CHAR, i, TAG, MPI_COMM_WORLD, &stat);
-       printf("%d: %s\n", myid, buff);
-     }
-   }
-   else
-   {
-     /* receive from rank 0: */
-     MPI_Recv(buff, BUFSIZE, MPI_CHAR, 0, TAG, MPI_COMM_WORLD, &stat);
-     sprintf(idstr, "Processor %d ", myid);
-     strcat(buff, idstr);
-     strcat(buff, "reporting for duty\n");
-     /* send to rank 0: */
-     MPI_Send(buff, BUFSIZE, MPI_CHAR, 0, TAG, MPI_COMM_WORLD);
+   if (myid == 0) {
+       cout << numprocs << " slaves are ready." << endl;
+       master();
+   } else {
+       slave(myid);
    }
  
-   MPI_Finalize(); /* MPI Programs end with MPI Finalize; this is a weak synchronization point */
+   MPI::Finalize(); /* MPI Programs end with MPI Finalize; this is a weak synchronization point */
    return 0;
  }
 
