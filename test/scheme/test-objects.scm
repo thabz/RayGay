585b5339586810fe8d8a56aaa5508a351f6b9a6c
Jesper Christensen <jesper@kalliope.org>
2009-11-25 20:48:03 +0000

    Testing

--- test/scheme/test-objects.scm
+++ test/scheme/test-objects.scm
@@ -12,7 +12,8 @@
  (cadr (intersect object origin direction)))
 
 (define (test-halfspace)
- (let ((y<10 (make-halfspace #(0 1 0) #(0 10 0) )))
+ (let ((y<10 (make-halfspace #(0 1 0) #(0 10 0)))
+       (y>5  (make-halfspace #(0 -1 0) #(0 5 0))))
  (test "Intersection test from outside" 
    (intersect? y<10 #(0 100 0) #(0 -1 0)))
  (test "Intersection from the outside" 
@@ -29,10 +30,12 @@
    (not (inside? y<10 #(0 11 0)))
    (inside? y<10 #(10 9 10))
    (inside? y<10 #(0 -9 0))))
- (test "Slab with CSG intersection of two halfplanes" 
-  (near-equal?
-   (intersect y<10 #(0 100 0) #(0 -1 0))
-   (list #(0 10 0) #(0 1 0))))
+ (test "Inside slab with CSG intersection of two halfplanes" 
+  (let ((slab (make-intersection y<10 y>5)))
+   (and
+    (not (inside? slab #(0 11 0)))
+    (inside? slab #(0 7 0))
+    (not (inside? slab #(0 4 0))))))
  (test "That 1 = 1" (= 1 1))))
 
 (run-test "Halfspaces" test-halfspace)
