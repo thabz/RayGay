26260622dd86b2ecf5ee65d78396c2abbaed5ee9
Jesper Christensen <jesper@kalliope.org>
2009-11-25 20:40:25 +0000

    More testing of halfspaces

--- test/scheme/test-objects.scm
+++ test/scheme/test-objects.scm
@@ -12,16 +12,27 @@
  (cadr (intersect object origin direction)))
 
 (define (test-halfspace)
- (let ((y<10 (make-halfspace #(0 10 0) #(0 1 0))))
- (test 
-  "Simple intersection" 
+ (let ((y<10 (make-halfspace #(0 1 0) #(0 10 0) )))
+ (test "Intersection test from outside" 
    (intersect? y<10 #(0 100 0) #(0 -1 0)))
- 
- (test 
-  "Simple intersection point" 
-  (equal?
-   (intersection-point y<10 #(0 100 0) #(0 -1 0))
-   #(0 10 0)))
+ (test "Intersection from the outside" 
+  (near-equal?
+   (intersect y<10 #(0 100 0) #(0 -1 0))
+   (list #(0 10 0) #(0 1 0))))
+ (test "Intersection from the inside" 
+  (near-equal?
+   (intersect y<10 #(0 -100 0) #(0 1 0))
+   (list #(0 10 0) #(0 1 0))))
+ (test "Points inside" 
+  (and
+   (inside? y<10 #(0 9 0))
+   (not (inside? y<10 #(0 11 0)))
+   (inside? y<10 #(10 9 10))
+   (inside? y<10 #(0 -9 0))))
+ (test "Slab with CSG intersection of two halfplanes" 
+  (near-equal?
+   (intersect y<10 #(0 100 0) #(0 -1 0))
+   (list #(0 10 0) #(0 1 0))))
  (test "That 1 = 1" (= 1 1))))
 
 (run-test "Halfspaces" test-halfspace)
--- test/scheme/test.scm
+++ test/scheme/test.scm
@@ -1,19 +1,47 @@
 
+;; ---------------------------------------------
+;; Helpers
+;; ---------------------------------------------
+
+(define (near-equal? a b)
+ (cond
+  ((and (list? a) (list? b))
+   (let loop ((l (map near-equal? a b)))
+    (cond
+     ((null? l) #t)
+     ((not (car l)) #f)
+     (else (loop (cdr l))))))
+  ((and (real? a) (real? b)) 
+   (> 0.0001 (abs (- a b))))
+  ((vector? a) 
+   (and
+    (near-equal? (vector-ref a 0) (vector-ref b 0))
+    (near-equal? (vector-ref a 1) (vector-ref b 1))
+    (near-equal? (vector-ref a 2) (vector-ref b 2))))))
+
+;; ---------------------------------------------
+;; The testing framework
+;; ---------------------------------------------
+
 (define tests-run 0)
 (define tests-failed 0)
+(define test-suite-name "")
 
 (define (test descr result)
  (set! tests-run (+ tests-run 1))
  (if (not result)
   (begin
    (set! tests-failed (+ tests-failed 1))
-   (display "Failed: ")
+   (display "FAILED ")
+   (display test-suite-name)
+   (display ":")
    (display descr)
    (newline))))
 
 (define (run-test name func)
  (set! tests-run 0)
  (set! tests-failed 0)
+ (set! test-suite-name name)
  (func)
  (display name)
  (display " ")
