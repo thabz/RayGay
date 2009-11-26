3c59261754c85691fa3e40bd26b9dd1c368101d9
Jesper Christensen <jesper@kalliope.org>
2009-11-26 15:11:44 +0000

    More

--- test/scheme/test-objects.scm
+++ test/scheme/test-objects.scm
@@ -20,8 +20,9 @@
 ;; ---------------------------------------------
 
 (define (test-halfspace)
- (let ((y<10 (make-halfspace #(0 1 0) #(0 10 0)))
-       (y>5  (make-halfspace #(0 -1 0) #(0 5 0))))
+ (let* ((y<10 (make-halfspace #(0 1 0) #(0 10 0)))
+        (y>5  (make-halfspace #(0 -1 0) #(0 5 0)))
+        (ybetween5and10 (make-intersection y<10 y>5)))
  (test "Intersection test from outside" 
    (intersect? y<10 #(0 100 0) #(0 -1 0)))
  (test "Intersection from the outside" 
@@ -39,11 +40,18 @@
    (inside? y<10 #(10 9 10))
    (inside? y<10 #(0 -9 0))))
  (test "Inside slab with CSG intersection of two halfplanes" 
-  (let ((slab (make-intersection y<10 y>5)))
    (and
-    (not (inside? slab #(0 11 0)))
-    (inside? slab #(0 7 0))
-    (not (inside? slab #(0 4 0))))))
+    (not (inside? ybetween5and10 #(0 11 0)))
+    (inside? ybetween5and10 #(0 7 0))
+    (not (inside? ybetween5and10 #(0 4 0)))))
+ (test "Intersect slab from over it"
+  (near-equal?
+   (intersect ybetween5and10 #(5 100 5) #(0 -1 0))
+   (list #(5 10 5) #(0 1 0))))
+ (test "Intersect slab from under it"
+  (near-equal?
+   (intersect ybetween5and10 #(1 -100 1) #(0 1 0))
+   (list #(1 5 1) #(0 -1 0))))
  (test "That 1 = 1" (= 1 1))))
 
 ;; ---------------------------------------------
