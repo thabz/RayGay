392748b34bcb0562410228e9dcb8304b3cc67991
Jesper Christensen <jesper@kalliope.org>
2009-11-29 13:40:50 +0000

    More work on (format)

--- test/scheme/test-format.scm
+++ test/scheme/test-format.scm
@@ -4,8 +4,16 @@
   (test "Passthrough" (equal? (format #f "Foo") "Foo"))
   (test "Format decimal" (equal? (format #f "Foo~dbar" 255) "Foo255bar"))
   (test "Format hex" (equal? (format #f "Foo~xbar" 255) "Fooffbar"))
+  (test "Format octal" (equal? (format #f "Foo~obar" 16) "Foo20bar"))
   (test "Format binary" (equal? (format #f "Foo~bbar" 3) "Foo11bar"))
   (test "Format list" (equal? (format #f "Foo~abar" '(1 2)) "Foo(1 2)bar"))
+  (test "Format char with a" (equal? (format #f "Foo~abar" #\x) "Fooxbar"))
+  (test "Format char with c" (equal? (format #f "Foo~cbar" #\x) "Fooxbar"))
+  (test "Escaped ~" (equal? (format #f "Foo~~bar") "Foo~bar"))
+  (test "Escaped space" (equal? (format #f "Foo~_bar") "Foo bar"))
+  (test "Escaped tab" (equal? (format #f "Foo~tbar") "Foo\tbar"))
+  (test "Escaped newline" (equal? (format #f "Foo~%bar") "Foo\nbar"))
+  (test "Embed string" (equal? (format #f "Foo~abar" "ss") "Foossbar"))
   (test "1 = 1" (= 1 1)))
 
 
