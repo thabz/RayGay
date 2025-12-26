b5b76538649760b4be99b84d603aee7ea8bc2ee1
Jesper Christensen <jesper@kalliope.org>
2025-12-26 08:36:01 +0100

    Fix Larry

--- scenes/larry.scm
+++ scenes/larry.scm
@@ -1,42 +1,41 @@
-
 (load "lib/raygay.scm")
 
 (set-image-size '(1024 1024))
-(set-background '(0.3 0.6 0.7))
+(set-background #(0.3 0.6 0.7))
 
 (set-renderer "raytracer")
 (set-camera 
   (make-pinhole-camera 
-    '( pos (10 1000 3000)
-       lookat (0 0 0)
-       up (0 1 0)
+    '( pos #(10 1000 3000)
+       lookat #(0 0 0)
+       up #(0 1 0)
        fov 45
        aa 4)))
 
 (define brown
   (make-material
-    '( diffuse (0.7 0.4 0.2)
+    '( diffuse #(0.7 0.4 0.2)
        kd 1.0
        ks 0.0)))
 
 (define grey85
   (make-material
-    '( diffuse (0.85 0.85 0.85)
+    '( diffuse #(0.85 0.85 0.85)
        kd 1.0
        ks 0.0)))
 
 (define chrome
   (make-material
-    '( diffuse (0.8 0.8 0.8)
+    '( diffuse #(0.8 0.8 0.8)
        kd 0.2
-       specular (1.0 1.0 1.0)
+       specular #(1.0 1.0 1.0)
        ks 0.8
        specpow 30)))
 
 
-(add-to-scene (make-pointlight '(500 1300 1300)))
+(add-to-scene (make-pointlight #(500 1300 1300)))
 
-(define img (make-texture "gfx/larry.jpg" 1.0 1.0 "none"))
+(define img (make-texture "gfx/larry.jpg" 1.0 1.0 'none))
 
 (define num 48)
 
@@ -45,8 +44,7 @@
   (do ((x (- num) (+ 1 x)))
     ((= x num))
     (add-to-scene
-      (make-sphere
-	(list (* x 20) (* y -20) 0) 15.0
+      (make-sphere `#(,(* x 20) ,(* y -20) 0) 15.0
 	(make-material 
 	  (list 
 	    'diffuse 
