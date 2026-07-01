f2ec5f0a68474903db8054b10afb26180a080e04
Jesper Christensen <jesper@kalliope.org>
2026-07-01 23:20:47 +0200

    Speed up testobjects transparent check (#39)
    * Speed up testobjects transparent check
    
    * Reduce testobjects stress sample count

--- test/testobjects.cpp
+++ test/testobjects.cpp
@@ -40,6 +40,8 @@ bool intersects(Object *o, const Vector &origin, const Vector &dir) {
   return intersects(o, ray);
 }
 
+static const int STRESS_CHECK_SAMPLES = 100;
+
 Vector iPoint(Object *o, const Ray &ray) {
   double t = o->fastIntersect(ray);
   if (t <= 0) {
@@ -79,9 +81,7 @@ Vector iNormal(Object *o, const Vector &origin, const Vector &dir) {
  */
 bool intersectionCheck(Object *object, double radius) {
   int failures = 0;
-  int num = 10000;
-  int checked = 0;
-  for (int i = 0; i < num; i++) {
+  for (int i = 0; i < STRESS_CHECK_SAMPLES; i++) {
     Vector v = Vector::randomUnitVector();
     Vector pos = v * radius;
     Vector dir = -1 * v;
@@ -90,9 +90,7 @@ bool intersectionCheck(Object *object, double radius) {
     if (t <= 0 || t > radius) {
       failures++;
     }
-    checked++;
   }
-  // cout << "Checked points: " << checked << endl;
   return failures == 0;
 }
 
@@ -101,10 +99,8 @@ bool intersectionCheck(Object *object, double radius) {
  * that the returned normals all have length one.
  */
 bool normalCheck(Object *object, double radius) {
-  int num = 10000;
-  int checked = 0;
   int failures = 0;
-  for (int i = 0; i < num; i++) {
+  for (int i = 0; i < STRESS_CHECK_SAMPLES; i++) {
     Vector v = Vector::randomUnitVector();
     Vector pos = v * radius;
     Vector dir = -1 * v;
@@ -113,10 +109,8 @@ bool normalCheck(Object *object, double radius) {
       if (!IS_EQUAL(iNormal(object, ray).length(), 1.0)) {
         failures++;
       };
-      checked++;
     }
   }
-  // cout << "Checked normals: " << checked << endl;
   return failures == 0;
 }
 
@@ -128,23 +122,23 @@ bool normalCheck(Object *object, double radius) {
  */
 bool transparentCheck(Object *object, double radius) {
   int failures = 0;
-  int num = 10000;
-  int checked = 0;
-  for (int i = 0; i < num; i++) {
+  for (int i = 0; i < STRESS_CHECK_SAMPLES; i++) {
     Vector v = Vector::randomUnitVector();
     Vector pos = v * radius;
     Vector dir = -1 * v;
     Ray ray = Ray(pos, dir, -1);
-    while (intersects(object, ray)) {
-      if (iPoint(object, ray) == pos)
+    double t;
+    while ((t = object->fastIntersect(ray)) > 0) {
+      Intersection intersection;
+      object->fullIntersect(ray, t, intersection);
+      Vector point = intersection.getPoint();
+      if (point == pos)
         failures++;
 
-      pos = iPoint(object, ray);
+      pos = point;
       ray = Ray(pos, dir, -1);
-      checked++;
     }
   }
-  // cout << "Checked points: " << checked << endl;
   return failures == 0;
 }
 
